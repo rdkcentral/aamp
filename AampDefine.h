492577a6c8d8d54771df11655acf5ed09510a6cf
shripad bankar <Shripad_Bankar@cable.comcast.com>
2023-05-28 12:09:02 -0400

    RDKAAMP-1143 : Remove "AAMP + Version" string append to user agent (when manually set)
    Reason for change: As per foxtel requirment, Foxtel dont want AAMP version appended to
    userAgent, hence when Applicaiton sets UserAgent Init config, aamp just uses app configured useragent and dont
    append AAMP version
    
    Test Procedure: refer test guidance in ticket
    
    Risks: Low
    
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Change-Id: I563e02c9a5560e8d141f2d6b22a37de218bbad8f
    (cherry picked from commit 654ff1a79625a9dc03bae54b2ec798c3aaa48d3e)

--- AampDefine.h
+++ AampDefine.h
@@ -100,7 +100,7 @@
 #define MAX_SEG_DRM_DECRYPT_FAIL_COUNT 10           		/**< Max segment decryption failures to identify a playback failure. */
 #define MAX_SEG_INJECT_FAIL_COUNT 10                		/**< Max segment injection failure to identify a playback failure. */
 #define AAMP_USERAGENT_BASE_STRING	"Mozilla/5.0 (Linux; x86_64 GNU/Linux) AppleWebKit/601.1 (KHTML, like Gecko) Version/8.0 Safari/601.1 WPE"	/**< Base User agent string,it will be appneded with AAMP_USERAGENT_SUFFIX */
-#define AAMP_USERAGENT_SUFFIX		"AAMP/"+AAMP_VERSION    /**< Version string of AAMP Player */
+#define AAMP_USERAGENT_SUFFIX		" AAMP/"+AAMP_VERSION    /**< Version string of AAMP Player */
 #define DEFAULT_AAMP_ABR_THRESHOLD_SIZE (6000)			/**< aamp abr threshold size */
 #define DEFAULT_PREBUFFER_COUNT (2)
 #define AAMP_LOW_BUFFER_BEFORE_RAMPDOWN 10 			/**< 10sec buffer before rampdown */
