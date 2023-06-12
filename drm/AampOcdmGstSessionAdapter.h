d284fce673835c08d50f59bf4ca369de990564f1
sputhi200 <Sujeesh_Puthiya@comcast.com>
2023-06-12 20:35:55 +0000

    LLAMA-10079 Added generic timecode parsing code
    Reason for change: Timecode parsing method is not working after gstreamer upgrade
    Test Procedure: Play any linear content and timecode are correctly reporting to application
    Risks: Low
    
    Signed-off-by: sputhi200 <Sujeesh_Puthiya@comcast.com>
    Change-Id: I2e9880fb5f0e953c5923af017cb4828a442df775
    (cherry picked from commit f9ae615dd97f0056619fac8f0819ebd84c34a292)

--- drm/AampOcdmGstSessionAdapter.h
+++ drm/AampOcdmGstSessionAdapter.h
@@ -17,9 +17,7 @@
 
 class AAMPOCDMGSTSessionAdapter : public AAMPOCDMSessionAdapter
 {
-#ifdef AMLOGIC
         void ExtractSEI( GstBuffer *buffer);
-#endif
 public:
 	AAMPOCDMGSTSessionAdapter(AampLogManager *logObj, std::shared_ptr<AampDrmHelper> drmHelper,  AampDrmCallbacks *drmCallbacks) : AAMPOCDMSessionAdapter(logObj, drmHelper, drmCallbacks)
 #if defined(AMLOGIC)
