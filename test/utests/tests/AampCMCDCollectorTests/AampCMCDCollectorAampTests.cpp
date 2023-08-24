e4f57a511ecd4ca2bc599f8ff1defa18c51d8463
ssalim378 <sohil_salimmiyanmalek@comcast.com>
2023-08-24 16:22:31 +0530

    RDKAAMP-1357 :[AAMP] utest completion for AampCMCDCollector Class
    Reason for change: Adding utest for AampCMCDCollector Class
    Test Procedure : Refer jira ticket
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_23_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_23_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_23_1
    
    Signed-off-by: ssalim378 <sohil_salimmiyanmalek@comcast.com>
    Change-Id: I08d1b138708c1502b01d687e98171575201977aa

--- test/utests/tests/AampCMCDCollectorTests/AampCMCDCollectorAampTests.cpp
+++ test/utests/tests/AampCMCDCollectorTests/AampCMCDCollectorAampTests.cpp
@@ -2,7 +2,7 @@
 * If not stated otherwise in this file or this component's license file the
 * following copyright and licenses apply:
 *
-* Copyright 2023 RDK Management
+* Copyright 2022 RDK Management
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -16,11 +16,12 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 */
-#include "AudioCMCDHeaders.h"
 
-void AudioCMCDHeaders::BuildCMCDCustomHeaders(std::unordered_map<std::string, std::vector<std::string>> &mCMCDCustomHeaders)
+#include <gtest/gtest.h>
+#include "AampCMCDCollector.h"
+
+int main(int argc, char** argv)
 {
-}
-void CMCDHeaders::SetNextRange(const std::string &CMCDNextRangeRequest)
-{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
