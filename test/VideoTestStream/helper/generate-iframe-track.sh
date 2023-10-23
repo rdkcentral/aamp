046123a0aa5c1aee1d4b08d9536aea8d9a8f2434
Phil Stroffolino <pstroffo@yahoo.com>
2023-10-23 14:14:20 -0400

    RDKAAMP-1693 generated test stream more similar to Viper Packaged production DASH
    Reason for change: fps and iframe track cadence change
    Test Procedure: ensure seek, trickplay still working as expected
    Risks: Low
    
    Change-Id: I19b061abb7216a66e9cbcfa4d8065bb156d08e84
    (cherry picked from commit eae51b108a73b58269bf0ec98e794d0bbce05a13)

--- test/VideoTestStream/helper/generate-iframe-track.sh
+++ test/VideoTestStream/helper/generate-iframe-track.sh
@@ -6,15 +6,16 @@ FILE_OVERLAY=overlayiframe.mp4
 ffmpeg -y -i loop.mp4 -vf \
 drawtext="fontfile=/path/to/font.ttf: text='%{pts\:hms}': fontcolor=white: fontsize=48: box=1: boxcolor=black: boxborderw=5: x=(w-text_w)/2: y=(h-text_h)/2",\
 drawtext="fontfile=/path/to/font.ttf: text='IFRAME ${HEIGHT[$I]}p': fontcolor=white: fontsize=48: box=1: boxcolor=black: boxborderw=5: x=(w-text_w)/2: y=(h/2-text_h)/2" \
--codec:a copy -g $((FPS*IFRAME_CADENCE_SEC)) $FILE_OVERLAY
+-codec:a copy -g $((GOP_SIZE)) $FILE_OVERLAY
 # single segment duration: IFRAME_CADENCE_SEC
 # single segment FPS: 1/IFRAME_CADENCE_SEC
 # what gives one I-Frame per segment and nothing else
 # Generate iframe fragments and playlist for hls
-ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $((1/IFRAME_CADENCE_SEC)) -g 1 -hls_time $IFRAME_CADENCE_SEC -hls_playlist_type vod -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -hls_segment_filename $HLS_OUT/iframe_%03d.ts $HLS_OUT/iframe.m3u8
+ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $IFRAME_RATE -g 1 -hls_time $IFRAME_CADENCE_SEC -hls_playlist_type vod -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -hls_segment_filename $HLS_OUT/iframe_%03d.ts $HLS_OUT/iframe.m3u8
 
 # Generate iframe fragments using hls fmp4 extension with HLS manifest
-ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $((1/IFRAME_CADENCE_SEC)) -g 1 -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -hls_segment_type fmp4 -hls_time $IFRAME_CADENCE_SEC -hls_playlist_type vod -hls_segment_filename $DASH_OUT/iframe_'%03d.m4s' -hls_fmp4_init_filename iframe_init.m4s -start_number 1 $DASH_OUT/iframe.m3u8
+ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $IFRAME_RATE -g 1 -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -hls_segment_type fmp4 -hls_time $IFRAME_CADENCE_SEC -hls_playlist_type vod -hls_segment_filename $DASH_OUT/iframe_'%03d.m4s' -hls_fmp4_init_filename iframe_init.m4s -start_number 1 $DASH_OUT/iframe.m3u8
 
 # override iframe fragments with the same ones and generate DASH manifest
-ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $((1/IFRAME_CADENCE_SEC)) -g 1 -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -seg_duration $((IFRAME_CADENCE_SEC)) -use_timeline 1 -use_template 1 -init_seg_name $DASH_OUT/iframe_init.m4s -media_seg_name $DASH_OUT/iframe_'$Number%03d$.m4s' -f dash iframe.mpd
+
+ffmpeg -hide_banner -y -i overlayiframe.mp4 -map 0:v -vf scale=w=${WIDTH[$I]}:h=${HEIGHT[$I]}:force_original_aspect_ratio=decrease,select='eq(pict_type\,PICT_TYPE_I)' -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -r $IFRAME_RATE -g 1 -b:v ${KBPS[$I]}k -maxrate ${MAXKBPS[$I]}k -bufsize 1200k -seg_duration $((IFRAME_CADENCE_SEC)) -use_timeline 1 -use_template 1 -init_seg_name $DASH_OUT/iframe_init.m4s -media_seg_name $DASH_OUT/iframe_'$Number%03d$.m4s' -f dash iframe.mpd
