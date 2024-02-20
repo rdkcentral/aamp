06af7397635594d399be66df91e6373c95fd958e
Rajat Yadav <ryadav983@resml-1726818.cable.comcast.com>
2024-02-20 16:36:05 -0500

    RDKAAMP-2125 [AAMP] L1/utest compositein_shimAamptest traps when address sanitized enabled
    Reason for change: To fix issues with uninitialized variable causing ASan error with compositein_shimAamptest.
    Test Procedure: Verify aamp-cli on OSX with file locators from generated test inputs for compositein_shimAamp tests.
    
    Risks: Low
    Priority: P0
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_1
    
    Change-Id: I2217549d601a227550afc6cf37407b9765edd9d0
    Signed-off-by: Rajat Yadav <ryadav983@resml-1726818.cable.comcast.com>

--- test/utests/fakes/FakeStreamAbstractionAamp.cpp
+++ test/utests/fakes/FakeStreamAbstractionAamp.cpp
@@ -22,7 +22,7 @@
 
 MockStreamAbstractionAAMP *g_mockStreamAbstractionAAMP = nullptr;
 
-StreamAbstractionAAMP::StreamAbstractionAAMP(AampLogManager *logObj, PrivateInstanceAAMP* aamp, id3_callback_t mID3Handler)
+StreamAbstractionAAMP::StreamAbstractionAAMP(AampLogManager *logObj, PrivateInstanceAAMP* aamp, id3_callback_t mID3Handler) : aamp(nullptr)
 {
     mLogObj = logObj;
 }
