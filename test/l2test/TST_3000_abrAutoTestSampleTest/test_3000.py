5d254dce623a84ae955faa766fb17e7a7f47e9fd
Patrick Conduit <pconduit@synamedia.com>
2024-02-29 11:29:45 +0000

    RDKAAMP-2242 [l2test] rename run_test.py files to unique names and remove __init__.py's
    Reason for change: Tidy up of test directory struct. Rename files and delete unused. Full explanation in jira
    
    Test Procedure: Run l2 tests, they should give same set of results
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_1
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_1
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_1
    
    Change-Id: Ic267b0ba740bf633218906a28ffd863aa06ec695
    Signed-off-by: Patrick Conduit <pconduit@synamedia.com>

--- test/l2test/TST_3000_abrAutoTestSampleTest/test_3000.py
+++ test/l2test/TST_3000_abrAutoTestSampleTest/test_3000.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
-#
+
 # If not stated otherwise in this file or this component's LICENSE file the
 # following copyright and licenses apply:
 #
-# Copyright 2023 Synamedia Ltd.
-#
+# Copyright 2023 RDK Management
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,7 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
+
 #Starts simlinear, a web server for serving ABR test streams
 #Starts aamp-cli and initiates playback by giving it a stream URL
 #verifys aamp log output against expected list of events
