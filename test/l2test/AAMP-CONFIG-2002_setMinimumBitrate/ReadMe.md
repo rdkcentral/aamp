0b378c650ec1512d6fa61030445e2c4d20696cb1
anurag krishnan <akrish513@cable.comcast.com>
2024-03-20 19:16:50 +0000

    RDKAAMP-2274:[AAMP] L2 Test - Test case to validate SetMinimumBitrate() API
    Reason for change:L2 test case for SetMinimumBitrate.
    
    Test Procedure: Test case updated in the ticket
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_2
    
    Risks: low
    Change-Id: I7e2c7e47863eb5463ee78ee5cd1ed7dca1e8b49e
    Signed-off-by: anurag_krishnan@comcast.com

---
+++ test/l2test/AAMP-CONFIG-2002_setMinimumBitrate/ReadMe.md
@@ -0,0 +1,26 @@
+# AAMP Multi profile test
+
+This python3 L2 test verifies that minimumBitrate config is working successfully
+
+<RDK-2274> L2 Test - Test case to validate SetMinimumBitrate() API
+
+## Pre-requisites to L2 tests:
+
+AAMP installed using install-aamp.sh script.
+
+
+This test plays the following streams:
+
+https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.m3u8
+https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.mpd
+
+and verifies that proper bitrate profiles are played correctly for 20s each.
+
+## Run l2test using script:
+
+
+## Example:
+
+    cd aamp/test/l2test/
+    source l2venv/bin/activate
+    ./run_l2_aamp.py -t 2002
---
+++ test/l2test/AAMP-CONFIG-2002_setMinimumBitrate/test_2002.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+# If not stated otherwise in this file or this component's LICENSE file the
+# following copyright and licenses apply:
+#
+# Copyright 2023 RDK Management
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Starts aamp-cli and initiates playback by giving it a stream URL
+# verifies aamp log output against expected list of events
+
+
+import os
+import sys
+import re
+import pytest
+from inspect import getsourcefile
+
+TESTDATA0 = {
+    "title": "Test SetMinimumBitrate() API. Set a valid value",
+    "max_test_time_seconds": 30,
+    "aamp_cfg": "info=true\ntrace=true\nabr=false\n",
+    "expect_list": [
+	{"cmd": "set minimumBitrate 3000000"},
+        {"expect": "set minimumBitrate"},
+        {"cmd": "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.m3u8"},
+        {"expect": "Video Profile added to ABR, userData=3 BW=5000000"},
+	{"expect": "Inserted. url https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/hls/1080p.m3u8"},
+	{"expect": "AAMP_EVENT_TUNED"},
+        {"expect": "IP_AAMP_TUNETIME"},
+        {"expect": "\"vfb\":5000000"},
+        {"cmd": "stop"},
+        {"expect": "aamp_stop PlayerState"},
+        {"cmd": "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.mpd"},
+	{"expect": "Added Video Profile to ABR BW= 5000000"},
+	{"expect": "Inserted init url https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/dash/1080p_init.m4s"},
+	{"expect": "AAMP_EVENT_TUNED"},
+        {"expect": "IP_AAMP_TUNETIME"},
+        {"expect": "\"vfb\":5000000"},
+        {"cmd": "stop"},
+        {"expect": "aamp_stop PlayerState"}
+    ]
+}
+
+TESTDATA1 = {
+    "title": "Test SetMinimumBitrate() API. Set an invalid value",
+    "max_test_time_seconds": 45,
+    "aamp_cfg": "info=true\ntrace=true\nabr=false\n",
+    "expect_list": [
+        {"cmd": "set minimumBitrate -30000"},
+        {"expect": "Invalid bitrate value"},
+        {"cmd": "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.m3u8"},
+        {"expect": "Inserted. url https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/hls/480p.m3u8"},
+	{"expect": "AAMP_EVENT_TUNED"},
+        {"expect": "IP_AAMP_TUNETIME"},
+        {"expect": "\"vfb\":1400000"},
+        {"cmd": "sleep 1"},
+	{"cmd": "get videoBitrates"},
+	{"expect": re.escape("VIDEO BITRATES = [ 800000, 1400000, 2800000, 5000000,  ]")},
+	{"cmd": "stop"},
+        {"expect": "aamp_stop PlayerState"},
+        {"cmd": "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/main.mpd"},
+        {"expect": "Inserted init url https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/dash/480p_init.m4s"},
+	{"expect": "AAMP_EVENT_TUNED"},
+        {"expect": "IP_AAMP_TUNETIME"},
+       	{"expect": "\"vfb\":1400000"},
+	{"cmd": "sleep 1"},
+	{"cmd": "get videoBitrates"},
+	{"expect": re.escape("VIDEO BITRATES = [ 5000000, 2800000, 1400000, 800000,  ]")},
+        {"cmd": "stop"},
+        {"expect": "aamp_stop PlayerState"}
+    ]
+}
+
+TESTLIST = [TESTDATA0,TESTDATA1]
+
+############################################################
+"""
+With this fixture we cause the test to be called 
+with each entry in TESTLIST
+"""
+@pytest.fixture(params=TESTLIST)
+def test_data(request):
+    return request.param
+
+def test_2002(aamp_setup_teardown,test_data):
+    aamp = aamp_setup_teardown
+    aamp.set_paths(os.path.abspath(getsourcefile(lambda: 0)))
+    aamp.run_expect_a(test_data)
