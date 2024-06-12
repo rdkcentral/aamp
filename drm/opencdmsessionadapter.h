3c386bb4908abed38338755f7aaf3a08fe040f43
Phil Stroffolino <pstroffo@yahoo.com>
2024-06-12 10:21:35 -0400

    RDKAAMP-2923 pthread_condattr_setclock(&condAttr, CLOCK_MONOTONIC ) gives compilation failure on OSX
    Reason for change: osx-apecific utest regression
    Test Procedure: run IsoBmffProcessorTestss on OSX
    Risks: Low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_2
    
    Change-Id: Id957135d57e005aafa6f7e5b450898e0b34996b0

--- drm/opencdmsessionadapter.h
+++ drm/opencdmsessionadapter.h
@@ -31,7 +31,7 @@ public:
 	Event() : signalled(false), lock(PTHREAD_MUTEX_INITIALIZER), condition(PTHREAD_COND_INITIALIZER), condAttr() {
                pthread_mutex_init(&lock, NULL);
                pthread_condattr_init(&condAttr);
-#ifndef __APPLE__
+#ifndef __APPLE__ // pthread_condattr_setclock API not available on OSX
                pthread_condattr_setclock(&condAttr, CLOCK_MONOTONIC );
 #endif
                pthread_cond_init(&condition, &condAttr);
