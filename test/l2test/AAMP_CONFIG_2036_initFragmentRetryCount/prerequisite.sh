d1b7f84d92a10f59fd7eb2f0f2449fa6866197a3
Anusha_Hiremath <Anusha_Hiremath@comcast.com>
2024-08-07 17:00:04 +0530

    RDKAAMP-3025 :[l2test] Test case to validate initFragmentRetryCount config
    Reason for Change: l2 test case
    
    Test Procedure : Refer jira ticket
    
    Risks: low
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_2
    
    Change-Id: I639f2783296f70d9123aeccd7691924af9d294a5
    Signed-off-by: Anusha_Hiremath <Anusha_Hiremath@comcast.com>

---
+++ test/l2test/AAMP_CONFIG_2036_initFragmentRetryCount/prerequisite.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -x
+
+echo $(pwd)
+mkdir -p $(pwd)/testdata
+
+download_stream() {
+
+    if [ "$TEST_2035_STREAM_PATH" == "" ]; then
+        TEST_2035_STREAM_PATH="https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/public/aamptest/testApps/L2/AAMP-IFRAME-4007/VideoTestStream.tar.xz"
+    fi
+
+    #Stop multiple fetching of data if we already have it
+    file=$(basename ${TEST_2035_STREAM_PATH})
+    if [ -f $file ]; then
+        echo "$file exists, not fetching again"
+        # exit 0
+    fi
+    if (curl -O  -X GET $TEST_2035_STREAM_PATH);then
+        echo "Test referring :" $TEST_2035_STREAM_PATH
+    else
+        echo "Downloading the stream failed. Check that the URL '$TEST_2035_STREAM_PATH' can be reached.
+        If the URL is incorrect, set the environmental variable TEST_2035_STREAM_PATH to the right value in .env file under l2test folder"
+    fi
+
+}
+files=$(find "$(pwd)/testdata" -maxdepth 1 -type f)
+if [ -z "$files" ]; then
+    echo "No files found directly under directory '$directory'."
+else
+    exit 0
+fi
+
+for x in $(pwd)/*.tar.xz
+do
+echo "extracting $x"
+if ! tar -xvf "$x" -C "$(pwd)/testdata"; then
+    echo "Error: '$x' is corrupted or not a valid zip archive."
+    rm -rf $x
+    download_stream
+else
+    echo "extraction complete ----> $x"
+fi
+done
