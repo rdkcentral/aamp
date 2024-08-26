80dd3c81ffc8e72c355eb79aa76a140362b65718
Mark Buser <mark_buser@comcast.com>
2024-08-26 11:29:13 -0400

    RDKAAMP-3219 VideoTestStream verify options/usage message/README
    Reason for change: Fix command line options, usage message, and README.
    Test Procedure: Run generate-hls-dash.sh -d 30 -k -l 0 -w. Should not give an error.
    
    Risks: Low
    Priority: P2
    
    DEPS_BRANCH=rdk/components/generic/gst-plugins-rdk-aamp:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampabr:dev_sprint_24_2
    DEPS_BRANCH=rdk/components/generic/aampmetrics:dev_sprint_24_2
    
    Change-Id: Ib8116c13073b49581b0a3734179406938a353f9a
    Signed-off-by: Mark Buser <mark_buser@comcast.com>

--- test/VideoTestStream/clean.sh
+++ test/VideoTestStream/clean.sh
@@ -6,16 +6,17 @@ rm -rf audio
 rm -rf dash
 rm -rf hls
 rm -rf text
-rm *.mp4
-rm *.ts
-rm muxed.m3u8
-rm SegmentBase.mpd
-rm SegmentList.mpd
-rm SegmentTemplate.mpd
-rm SegmentTimeline.mpd
-rm SegmentTimeline4k.mpd
-rm *p.mpd
-rm main.mpd
-rm main.m3u8
-rm main_mp4.m3u8 
-rm main_mux.m3u8
+rm -f *.mp4
+rm -f *.ts
+rm -f muxed.m3u8
+rm -f SegmentBase.mpd
+rm -f SegmentList.mpd
+rm -f SegmentTemplate.mpd
+rm -f SegmentTimeline.mpd
+rm -f SegmentTimeline4k.mpd
+rm -f *p.mpd
+rm -f main.mpd
+rm -f main.m3u8
+rm -f main_mp4.m3u8 
+rm -f main_mux.m3u8
+echo "VideoTestStream cleaned"
