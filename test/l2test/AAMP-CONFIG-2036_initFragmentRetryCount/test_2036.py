e644590f076f0cdee66995f71593f94578f99b77
Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>
2025-01-04 00:27:33 +0530

    RDKAAMP-3792: Fix AAMP ABR buffer based rampdown
    Reason for change: Remove unused HybridABRManager function
    IsLowestProfile(). Modify L2 test 2036 to handle initfragment retry
    attempt and subsequent playback related logs. Refactor AAMP buffer
    based ABR rampdown to be more proactive
    Test Procedure: L2 tests 80xx, 2036 should pass.
    Risks: Medium
    
    Change-Id: Ibf55b932153980905516b2c0470cc6eeb134e4da
    Signed-off-by: Vinish K B <Vinish_KorasseryBalakrishnan@comcast.com>

--- test/l2test/AAMP-CONFIG-2036_initFragmentRetryCount/test_2036.py
+++ test/l2test/AAMP-CONFIG-2036_initFragmentRetryCount/test_2036.py
@@ -28,10 +28,10 @@ import json
 archive_url = "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/public/aamptest/testApps/L2/AAMP-IFRAME-4007/VideoTestStream.tar.xz"
 
 """
-[{"delay": 5, "pattern": "(1080|720|480)p_init.m4s"}]
-Gives HTTP 404 error for all video segments numbered from 006 to 018
+[{"delay": 10000, "pattern": "(1080|720|480)p_init.m4s"}]
+Delays all video init segments downloads from 1080p, 720p by 10 seconds. Only 480p and 360p init segments will be unaffected
 """
-simlinearResp=[{"delay": 10000, "pattern": "(1080|720|480)p_init.m4s"}]
+simlinearResp=[{"delay": 10000, "pattern": "(1080|720)p_init.m4s"}]
 
 # data = "W3siZGVsYXkiOiAxMCwgInBhdHRlcm4iOiAiKDEwODB8NzIwfDQ4MClwX2luaXQubTRzIn1d"
 data = base64.b64encode(json.dumps(simlinearResp).encode('utf-8')).decode('utf-8')
@@ -40,52 +40,74 @@ initFragmentRetryCount = 2
 TESTDATA1 = {
     "title": "init Fragment retry count",
     "logfile": f"initFragmentRetryCount_{initFragmentRetryCount}.txt",
+    # 3 attempts of 4s each for 2 init fragments. Total 6 attempts are made
     "max_test_time_seconds": 60,
-    "aamp_cfg": f"info=true\ntrace=true\ninitFragmentRetryCount={initFragmentRetryCount}\ninitialBitrate=5000000",
+    "aamp_cfg": f"info=true\ntrace=true\ninitFragmentRetryCount={initFragmentRetryCount}\ninitialBitrate=5000000\nnetworkTimeout=4\n",
     "archive_url": archive_url,
     "url":f"VideoTestStream/main.mpd?respData={data}",
     "simlinear_type": "DASH",
     "expect_list": [ 
         {"expect": "AAMP_EVENT_STATE_CHANGED: INITIALIZING"},
+        # One original attempt and 2 retry attempts. Total 3 download attempts are actually made
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:1"},
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:2"},
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Init fragment fetch failed -- fragmentUrl http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
-        {"expect": "Not able to download init fragments; reached failure threshold sending tune failed event"},
-        {"expect": "AAMP: init fragment download failed"},
-        {"expect": "AAMP_EVENT_TUNE_FAILED reason=AAMP: init fragment download failed : Curl Error Code 28, Download time expired"},
+        {"expect": r"StreamAbstractionAAMP_MPD: ABR 1920x1080\[5000000\] -> 1280x720\[2800000\]"},
+        # Same happens with 720p init fragment as rampdown is in single step
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
+        {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:1"},
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
+        {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:2"},
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
+        {"expect": "Init fragment fetch failed -- fragmentUrl http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
+        {"expect": r"StreamAbstractionAAMP_MPD: ABR 1280x720\[2800000\] -> 842x474\[1400000\]"},
+        {"expect": r"HttpRequestEnd: 2,7,200.*?http://localhost:8085/VideoTestStream/dash/480p_init.m4s"},
+        {"expect": "AAMP_EVENT_STATE_CHANGED: PLAYING"},
+        # Wait for some more fragments to be downloaded
+        {"expect": r"HttpRequestEnd: 0,0,200.*?http://localhost:8085/VideoTestStream/dash/(480|360)p_010.m4s","end_of_test": True},
     ]
 }
+
 initFragmentRetryCount = 3
 TESTDATA2 = {
     "title": "init Fragment retry count",
     "logfile": f"initFragmentRetryCount_{initFragmentRetryCount}.txt",
-    "max_test_time_seconds": 60,
-    "aamp_cfg": f"info=true\ntrace=true\ninitialBitrate=2800000\ninitFragmentRetryCount={initFragmentRetryCount}\n",
+    "max_test_time_seconds": 80,
+    "aamp_cfg": f"info=true\ntrace=true\ninitialBitrate=2800000\ninitFragmentRetryCount={initFragmentRetryCount}\nnetworkTimeout=4\n",
     "archive_url": archive_url,
     "url":f"VideoTestStream/main.mpd?respData={data}",
     "simlinear_type": "DASH",
     "expect_list": [ 
         {"expect": "AAMP_EVENT_STATE_CHANGED: INITIALIZING"},
+        # # One original attempt and 3 retry attempts for 720p. Total 4 download attempts are actually made
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:1"},
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:2"},
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:3"},
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
         {"expect": "Init fragment fetch failed -- fragmentUrl http://localhost:8085/VideoTestStream/dash/720p_init.m4s"},
-        {"expect": r"AAMPLogABRInfo : switching to 'higher' profile '1 -> 0' currentBandwidth\[2800000\]->desiredBandwidth\[5000000\] nwBandwidth\[-1\] reason='fragment download failed' error='http error 28' symptom='video quality may increase \(or\) freeze/buffering'"},
+        {"expect": r"StreamAbstractionAAMP_MPD: ABR 1280x720\[2800000\] -> 842x474\[1400000\]"},
+        # 480p succeeds
+        {"expect": r"HttpRequestEnd: 2,7,200.*?http://localhost:8085/VideoTestStream/dash/480p_init.m4s"},
+        {"expect": "AAMP_EVENT_STATE_CHANGED: PLAYING"},
+        # Rampup happens once buffer builds up to the top profile
+        {"expect": r"StreamAbstractionAAMP_MPD: ABR 842x474\[1400000\] -> 1920x1080\[5000000\]"},
+        # One original attempt and 3 retry attempts for 1080p. Total 4 download attempts are actually made
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:1"},
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:2"},
         {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Download failed due to curl timeout or isDownloadStalled:0 Retrying:1 Attempt:3"},
+        {"expect": r"(?i)AAMPLogNetworkError error='curl error 28' type='INIT_VIDEO' location='unknown' symptom='video fails to start' url='http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
         {"expect": "Init fragment fetch failed -- fragmentUrl http://localhost:8085/VideoTestStream/dash/1080p_init.m4s"},
-        {"expect": "Not able to download init fragments; reached failure threshold sending tune failed event"},
-        {"expect": "AAMP: init fragment download failed"},
-        {"expect": "AAMP_EVENT_TUNE_FAILED reason=AAMP: init fragment download failed : Curl Error Code 28, Download time expired"},
+        # Wait for some more fragments to be downloaded
+        {"expect": r"HttpRequestEnd: 0,0,200.*?http://localhost:8085/VideoTestStream/dash/(480|360)p_010.m4s","end_of_test": True},
     ]
 }
 
