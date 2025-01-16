c30d5d2ee933a9511ab6ee0c64e44876eb715e4d
Gnanesha <Gnanesha_Thimmegowda@comcast.com>
2025-01-16 14:54:22 -0500

    RDKAAMP-3832 : XumoPlay app playError spike in QS035 (Aamp version: 6.12)
    Reason for change: remove the absolutechangeline check for Start Time
    to calculate see_pos_seconds so that position is updated properly
    
    Test Procedure :Play cinivalue westerns channel from xumo
    and no retune should occure around discontinuity
    Priority : P1
    Risks: Low
    
    Change-Id: Iea3e378d5aca417157a799df46beb31d8b6062b1
    Signed-off-by: Gnanesha <Gnanesha_Thimmegowda@comcast.com>

--- test/l2test/AAMP-CDAI-8012_SplitPeriodAd/test_8012.py
+++ test/l2test/AAMP-CDAI-8012_SplitPeriodAd/test_8012.py
@@ -155,7 +155,131 @@ TESTDATA4 = {
         {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_040.m4s\?live=true", "min": 60, "max": 150,"end_of_test":True},
     ]
 }
-TESTLIST = [TESTDATA1,TESTDATA2,TESTDATA3,TESTDATA4]
+
+TESTDATA5 = {
+    "title": "Test5 Split period ad without pts restamp",
+    # Test content is as follows : described as -> <N>th period <duration of period> seconds -> <scte35 marker duration> seconds ad
+    #  0th period 30s no ads, 1st period 20 seconds -> 20 second ad, 2nd period 10 seconds -> 20 second ad, 3rd period 10 seconds -> no ad, 4th period 20 seconds -> 20 second ad, 5th period no ads till end of content
+    #  current behaviour : after playing 20 second ad in 2nd period we see 3rd period (base content 01.00 to 01.10 timestamp) playback for 10 seconds .
+    #  expected behaviour : after playing 20 second ad in 2nd period we should see 4th period ad playback for 20 seconds
+    "max_test_time_seconds": 300,
+    "aamp_cfg": "client-dai=true\ninfo=true\nprogress=true\ndebug=true\nenablePTSReStamp=false\n",
+    "archive_url": archive_url,
+    'archive_server': {'server_class': WindowServer},
+    "url": "http://localhost:8080/content/split.mpd?live=true",
+    "cmdlist": [
+    "adtesting",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 0",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 1",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 2",
+    "advert list",
+    ],
+    # needs to be expanded.
+    "expect_list": [
+        {"expect": r"\[Tune\]\[\d+\]FOREGROUND PLAYER\[0\] aamp_tune: attempt: 1 format: DASH URL: http://localhost:8080/content/split.mpd", "min": 0, "max": 3},
+        {"expect": r"\[FoundEventBreak\]\[\d+\]\[CDAI\] Found Adbreak on period\[\d\] Duration\[\d+\]", "min": 0, "max": 150},
+        {"expect": r"\[onAdEvent\]\[\d+\]\[CDAI\]: State changed from \[\w+\] \=\> \[\w+\].", "min": 0, "max": 150},
+        {"expect": r"Detected split period. nextperioddur \= \d+.\d+ currperioddur \= \d+.\d+ currAd.duration \= \[\d+\] ", "min": 0, "max": 150},
+        #Expect not to download any fragments from 3rd period
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_031.m4s\?live=true", "min": 60, "max": 150,"not_expected" : True},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_061.m4s\?live=true", "min": 60, "max": 150},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_062.m4s\?live=true", "min": 60, "max": 150, "end_of_test":True},
+    ]
+}
+
+TESTDATA6 = {
+    "title": "Test6 Split period ad with short ad without pts restamp",
+    # Test content is as follows : described as -> <N>th period <duration of period> seconds -> <scte35 marker duration> seconds ad
+    #  0th period 30s no ads, 1st period 20 seconds -> 20 second ad, 2nd period 10 seconds -> 15 second ad, 3rd period 10 seconds -> no ad, 4th period 20 seconds -> 20 second ad, 5th period no ads till end of content
+    #  current behaviour : after playing 15 second ad in 2nd period we see 3rd period (base content 01.00 to 01.10 timestamp) playback for 10 seconds .
+    #  expected behaviour : after playing 15 second ad in 2nd period we should see 5 seconds of base content of 3rd period then 4th period ad playback for 20 seconds
+    "max_test_time_seconds": 300,
+    "aamp_cfg": "client-dai=true\ninfo=true\nprogress=true\ndebug=true\nenablePTSReStamp=false\n",
+    "archive_url": archive_url,
+    'archive_server': {'server_class': WindowServer},
+    "url": "http://localhost:8080/content/split2.mpd?live=true",
+    "cmdlist": [
+    "adtesting",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 0",
+    "advert add " + "http://localhost:8080/content/ad_15s.mpd" + " 15 1",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 2",
+    "advert list",
+    ],
+    # needs to be expanded.
+    "expect_list": [
+        {"expect": r"\[Tune\]\[\d+\]FOREGROUND PLAYER\[0\] aamp_tune: attempt: 1 format: DASH URL: http://localhost:8080/content/split2.mpd", "min": 0, "max": 3},
+        {"expect": r"\[FoundEventBreak\]\[\d+\]\[CDAI\] Found Adbreak on period\[\d\] Duration\[\d+\]", "min": 0, "max": 150},
+        {"expect": r"\[onAdEvent\]\[\d+\]\[CDAI\]: State changed from \[\w+\] \=\> \[\w+\].", "min": 0, "max": 150},
+        {"expect": r"Detected split period. nextperioddur \= \d+.\d+ currperioddur \= \d+.\d+ currAd.duration \= \[\d+\] ", "min": 0, "max": 150},
+        #Expect not to download any fragments from 3rd period
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_031.m4s\?live=true", "min": 60, "max": 150,"not_expected" : True},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_061.m4s\?live=true", "min": 60, "max": 150},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_062.m4s\?live=true", "min": 60, "max": 150, "end_of_test":True},
+    ]
+}
+
+#Split period ad across multiple periods
+TESTDATA7 = {
+    "title": "Test7 Split period ad across multiple periods without pts restamp ",
+    # Test content is as follows : described as -> <N>th period <duration of period> seconds -> <scte35 marker duration> seconds ad
+    #  0th period 30s no ads, 1st period 10 seconds -> 30 second ad, 2nd period 10 seconds ->no ad, 3rd period 10 seconds -> no ad, 4th period 20 seconds ->  no ad, 5th period no ads till end of content
+    #  Expected behaviour : after playing 30 second ad in 1ST period ,It should skip period 2,3 and should starts from period 4
+    "max_test_time_seconds": 300,
+    "aamp_cfg": "client-dai=true\ninfo=true\nprogress=true\ndebug=true\nenablePTSReStamp=false\n",
+    "archive_url": archive_url,
+    'archive_server': {'server_class': WindowServer},
+    "url": "http://localhost:8080/content/split3.mpd?live=true",
+    "cmdlist": [
+    "adtesting",
+    "advert add " + "http://localhost:8080/content/ad_30s.mpd" + " 30 0",
+    "advert list",
+    ],
+    # needs to be expanded.
+    "expect_list": [
+        {"expect": r"\[Tune\]\[\d+\]FOREGROUND PLAYER\[0\] aamp_tune: attempt: 1 format: DASH URL: http://localhost:8080/content/split3.mpd", "min": 0, "max": 3},
+        {"expect": r"\[FoundEventBreak\]\[\d+\]\[CDAI\] Found Adbreak on period\[\d\] Duration\[\d+\]", "min": 0, "max": 150},
+        {"expect": r"\[onAdEvent\]\[\d+\]\[CDAI\]: State changed from \[\w+\] \=\> \[\w+\].", "min": 0, "max": 150},
+        {"expect": r"Detected split period. nextperioddur \= \d+.\d+ currperioddur \= \d+.\d+ currAd.duration \= \[\d+\] ", "min": 0, "max": 150},
+        #Expect not to download fragments from 3rd period beginning
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_026.m4s\?live=true", "min": 60, "max": 150,"not_expected" : True},
+        #Expect to play the remaining content from base period 4
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_033.m4s\?live=true", "min": 60, "max": 150},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_040.m4s\?live=true", "min": 60, "max": 150,"end_of_test":True},
+    ]
+}
+
+#Split period ad across multiple periods
+TESTDATA8 = {
+    "title": "Test8 Split period ad across multiple periods without pts restamp",
+    # Test content is as follows : described as -> <N>th period <duration of period> seconds -> <scte35 marker duration> seconds ad
+    #  0th period 30s no ads, 1st period 10 seconds -> 30 second ad, 2nd period 10 seconds ->no ad, 3rd period 10 seconds -> no ad, 4th period 20 seconds ->  no ad, 5th period no ads till end of content
+    #  Expected behaviour : after playing 30 second ad in 1ST period ,It should skip period 2,3 and should starts from period 4
+    "max_test_time_seconds": 300,
+    "aamp_cfg": "client-dai=true\ninfo=true\nprogress=true\ndebug=true\nenablePTSReStamp=false\n",
+    "archive_url": archive_url,
+    'archive_server': {'server_class': WindowServer},
+    "url": "http://localhost:8080/content/split3.mpd?live=true",
+    "cmdlist": [
+    "adtesting",
+    "advert add " + "http://localhost:8080/content/ad_20s.mpd" + " 20 0",
+    "advert add " + "http://localhost:8080/content/ad_10s.mpd" + " 10 0",
+    "advert list",
+    ],
+    # needs to be expanded.
+    "expect_list": [
+        {"expect": r"\[Tune\]\[\d+\]FOREGROUND PLAYER\[0\] aamp_tune: attempt: 1 format: DASH URL: http://localhost:8080/content/split3.mpd", "min": 0, "max": 3},
+        {"expect": r"\[FoundEventBreak\]\[\d+\]\[CDAI\] Found Adbreak on period\[\d\] Duration\[\d+\]", "min": 0, "max": 150},
+        {"expect": r"\[onAdEvent\]\[\d+\]\[CDAI\]: State changed from \[\w+\] \=\> \[\w+\].", "min": 0, "max": 150},
+        {"expect": r"Detected split period. nextperioddur \= \d+.\d+ currperioddur \= \d+.\d+ currAd.duration \= \[\d+\] ", "min": 0, "max": 150},
+        #Expect not to download fragments from 3rd period beginning
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_026.m4s\?live=true", "min": 60, "max": 150,"not_expected" : True},
+        #Expect to play the remaining content from base period 4
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_033.m4s\?live=true", "min": 60, "max": 150},
+        {"expect": r"\[GetFile\]\[\d+\]aamp url:0,0,0,2.000000,http://localhost:8080/content/dash/(1080|720|480|360)p_040.m4s\?live=true", "min": 60, "max": 150,"end_of_test":True},
+    ]
+}
+
+TESTLIST = [TESTDATA1,TESTDATA2,TESTDATA3,TESTDATA4,TESTDATA5,TESTDATA6,TESTDATA7,TESTDATA8]
 @pytest.fixture(params=TESTLIST)
 def test_data(request):
     return request.param
