37c9f07f0e0be1e841f0c86e3c265bda02914a50
Kadungoth, Vinod (Contractor) <Vinod_Kadungoth2@comcast.com>
2025-02-11 14:53:50 -0500

    RDKAAMP-3952:[Charter]Playback skips end fragments and jumps to next period on VOD DAI test stream
    Reason for change:
    
    Period end time also needs to be updated according to the PTO if PTO is greater than the start time.
    
    Test Procedure: Test case updated in the ticket
    Risks: low
    Change-Id: I0a551909d9984348cdb54d7935f7f9922b5bb4b4
    Signed-off-by: vinod_kadungoth2@comcast.com

--- test/l2test/AAMP-TUNE-1007_AdInsertion/test_1007.py
+++ test/l2test/AAMP-TUNE-1007_AdInsertion/test_1007.py
@@ -22,69 +22,65 @@
 
 
 import os
-import sys
 import json
-import copy
 import pytest
-
 from inspect import getsourcefile
-
 
 DASH_TEST_STREAM = "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/public/aamptest/streams/DELIA-65821_Multiperiod/ad-insertion-testcase1/batch5/real/a/ad-insertion-testcase1.mpd"
 
-TESTS=[    {"cmd":'setconfig {"info":true,"progress":true,"jsinfo":true}'}
-]
+DASH_TEST_STREAM_1 = "https://cpetestutility.stb.r53.xcal.tv/VideoTestStream/public/aamptest/streams/L2/AAMP-TUNE-1007_AdInsertion/charter_dai/tl_dai.mpd"
+
+TESTS = [{"cmd": 'setconfig {"info":true,"progress":true,"jsinfo":true}'}]
 
 # Create instances
 playback = {}
 
-
-
-#Function to check if Skipping Fetchfragment logline from regular expression match
+# Function to check if Skipping Fetchfragment logline from regular expression match
 def check_skipFetchFragment(re_match):
-        current_directory = os.getcwd()
-        folder_name = 'AAMP-TUNE-1007_AdInsertion/output/'
-        file_name = 'test_1007[test_data0].log'
-        logfile_path = os.path.join(current_directory, folder_name, file_name) 
-        with open(logfile_path, 'r') as file:
-            log_content = file.read()
-            assert "Skipping Fetchfragment" not in log_content, "Test failed: 'Skipping Fetchfragment' found in log file"
+    current_directory = os.getcwd()
+    folder_name = 'AAMP-TUNE-1007_AdInsertion/output/'
+    file_name = 'test_1007[test_data0].log'
+    logfile_path = os.path.join(current_directory, folder_name, file_name)
+    with open(logfile_path, 'r') as file:
+        log_content = file.read()
+        assert "Skipping Fetchfragment" not in log_content, "Test failed: 'Skipping Fetchfragment' found in log file"
 
+# Dictionary to store basic test stream and sequence of commands that we need to verify
+TEST_DATA_1= {
+    "title": "TEST ADINSERTION",
+    "max_test_time_seconds": 30,
+    "expect_list": [
+        {"cmd": TESTS[0]['cmd']},
+        {"cmd": "progress"},
+        {"cmd": DASH_TEST_STREAM},
+        {"expect": "AAMP_EVENT_STATE_CHANGED: PLAYING"},
+        {"expect": r"position=26\.\d+", "callback": check_skipFetchFragment},
+        {"expect": "AAMP_EVENT_EOS"},
+    ]
+}
 
+# Test case where PTO is greater than the start time, and the delta between the first timeline value and PTO is greater than at least one fragment.
 
+TEST_DATA_2 = {
+    "title": "TEST PTO",
+    "max_test_time_seconds": 30,
+    "expect_list": [
+        {"cmd": "seek 812"},
+        {"cmd": TESTS[0]['cmd']},
+        {"cmd": "progress"},
+        {"cmd": DASH_TEST_STREAM_1},
+        {"expect": "AAMP_EVENT_STATE_CHANGED: PLAYING"},
+        {"expect": r"position=812\.\d+", "callback": check_skipFetchFragment},
+    ]
+}
 
+TESTLIST = [TEST_DATA_1,TEST_DATA_2]
 
-@pytest.fixture(params=TESTS)
+@pytest.fixture(params=TESTLIST)
 def test_data(request):
-    '''
-    This returns a test data of different configurations
-    '''
     return request.param
 
-@pytest.mark.ci_test_set
-def test_1007(aamp_setup_teardown,test_data):
-
-    #Dictionary to store basic test stream and sequence of commands that we need to verify
-    BASIC_TEST_DATA= {
-        "title": "TEST ADINSERTION ",
-        "max_test_time_seconds": 30,
-        "expect_list": [
-
-            {"cmd": test_data['cmd']},
-            {"cmd": "progress"},
-            {"cmd": DASH_TEST_STREAM},
-            {"expect": "AAMP_EVENT_STATE_CHANGED: PLAYING"},
-            {"expect": r"position=26\.\d+","callback":check_skipFetchFragment},
-            {"expect":"AAMP_EVENT_EOS"},
-    ]
-    }
-
-    # Update playback for each test
-    global playback
-    playback = copy.deepcopy(BASIC_TEST_DATA)
+def test_1007(aamp_setup_teardown, test_data):
     aamp = aamp_setup_teardown
     aamp.set_paths(os.path.abspath(getsourcefile(lambda: 0)))
-    #Passing each test sequence to aampcli through run_expect_a API
-    aamp.run_expect_a(playback)
-
-
+    aamp.run_expect_a(test_data)
