3d284136a53aca2f92f5df264873a5617bf64413
Paulpandian Mariappan <paulpandian_mariappan@cable.comcast.com>
2025-02-28 14:43:42 -0500

    XIONE-16675 : OTTPENDING observed after preroll adverts in VOD content
    Reason for change: reducing the cache count for puck devices
    preivously XIONE_UK is used however it is broken in 8.0.
    now using REALTEKCE since it is avilable from platform
    Test Procedure : please see the ticket
    Risks: Low
    
    Change-Id: I2a8788eff5b286d5c62b3c8b2226fe008bd4f3e4
    Signed-off-by: Gnanesha <Gnanesha_Thimmegowda@comcast.com>

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -921,12 +921,13 @@ void AampConfig::ApplyDeviceCapabilities( PlatformType platform )
 		case ePLATFORM_AMLOGIC:
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_NoNativeAV, true);
 			break;
-			
+
 		case ePLATFORM_REALTEK:
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_SyncAudioFragments, true);		// Handled in HLS::Init to avoid audio loss while seeking HLS/TS AV of different duration w/o affecting VOD Discontinuities
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_RequiredQueuedFrames, 3 + 1);
+			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_MaxFragmentCached, 3);
 			break;
-			
+
 		case ePLATFORM_BROADCOM:
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_DisableAC4, true);
 			if (!AAMPGstPlayer::IsMS2V12Supported())
@@ -935,7 +936,7 @@ void AampConfig::ApplyDeviceCapabilities( PlatformType platform )
 				SetConfigValue(AAMP_TUNE_SETTING, eAAMPConfig_EnableLiveLatencyCorrection, false);
 			}
 			break;
-			
+
 		case ePLATFORM_DEFAULT:
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_EnableLowLatencyCorrection, false);
 			SetConfigValue(AAMP_DEFAULT_SETTING, eAAMPConfig_UseWesterosSink, false );
