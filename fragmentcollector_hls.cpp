79228ef7bbcaa09231009e786e39bdd50e776fe0
DomSyna <192202460+DomSyna@users.noreply.github.com>
2025-07-01 17:53:16 +0000

    VPLAY-9393 bitrate (sometimes) remains low after throttling, then restoring bandwidth

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -1401,6 +1401,8 @@ bool TrackState::FetchFragmentHelper(int &http_error, bool &decryption_error, bo
 				// Track the end of buffer from the last downloaded fragment
 				// Use the playlistPosition instead of a rolling count in case segments are dropped
 				playTargetBufferCalc = playlistPosition + fragmentDurationSeconds;
+				AAMPLOG_MIL("DJH playTargetBufferCalc set to %f (playlistPosition %f + fragmentDurationSeconds %f)", 
+						playTargetBufferCalc.inSeconds(), playlistPosition.inSeconds(), fragmentDurationSeconds);
 			}
 
 			if((eTRACK_VIDEO == type)  && (aamp->IsFogTSBSupported()))
@@ -3239,6 +3241,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::SyncTracks(void)
 	}
 	// New calculated playTarget assign back for buffer calculation
 	video->playTargetBufferCalc = video->playTarget;
+	AAMPLOG_MIL("DJH video->playTargetBufferCalc set to %f", video->playTargetBufferCalc.inSeconds());
 	if (!trackState[eMEDIATYPE_AUDIO]->enabled)
 	{
 		AAMPLOG_WARN("Exit : aux track start %f, muxed track start %f sub track start %f",
@@ -4035,6 +4038,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 		{
 			trackState[iTrack]->playTarget = seekPosition;
 			trackState[iTrack]->playTargetBufferCalc = seekPosition;
+			AAMPLOG_MIL("DJH trackState[%d]->playTargetBufferCalc set to %f", iTrack, trackState[iTrack]->playTargetBufferCalc.inSeconds());
 		}
 
 		if ((video->enabled && video->mDuration == 0.0f) || (audio->enabled && audio->mDuration == 0.0f))
@@ -4221,20 +4225,24 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 				offsetToLive = (std::min)(offsetToLiveVideo,offsetToLiveAudio);
 				video->playTarget += offsetToLive;
 				video->playTargetBufferCalc = video->playTarget;
+				AAMPLOG_MIL("DJH video->playTargetBufferCalc set to %f", video->playTargetBufferCalc.inSeconds());
 				if (audio->enabled )
 				{
 					audio->playTarget += offsetToLive;
 					audio->playTargetBufferCalc = audio->playTarget;
+					AAMPLOG_MIL("DJH audio->playTargetBufferCalc set to %f", audio->playTargetBufferCalc.inSeconds());
 				}
 				if (subtitle->enabled)
 				{
 					subtitle->playTarget += offsetToLive;
 					subtitle->playTargetBufferCalc = subtitle->playTarget;
+					AAMPLOG_MIL("DJH subtitle->playTargetBufferCalc set to %f", subtitle->playTargetBufferCalc.inSeconds());
 				}
 				if (aux->enabled)
 				{
 					aux->playTarget += offsetToLive;
 					aux->playTargetBufferCalc = aux->playTarget;
+					AAMPLOG_MIL("DJH aux->playTargetBufferCalc set to %f", aux->playTargetBufferCalc.inSeconds());
 				}
 				// Entering live will happen if offset is adjusted , if its 0 playback is starting from beginning
 				if(offsetToLive != 0.0)
@@ -4295,6 +4303,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 										video->playTarget.inSeconds(), videoPrevDiscontinuity.inSeconds());
 								video->playTarget = videoPrevDiscontinuity;
 								video->playTargetBufferCalc = video->playTarget;
+								AAMPLOG_MIL("DJH video->playTargetBufferCalc set to %f (videoPrevDiscontinuity)", video->playTargetBufferCalc.inSeconds());
 							}
 							if (otherTrack->playTarget < audioPrevDiscontinuity)
 							{
@@ -4302,6 +4311,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 										otherTrack->name, otherTrack->playTarget.inSeconds(), audioPrevDiscontinuity.inSeconds());
 								otherTrack->playTarget = audioPrevDiscontinuity;
 								otherTrack->playTargetBufferCalc = otherTrack->playTarget;
+								AAMPLOG_MIL("DJH %s->playTargetBufferCalc set to %f (audioPrevDiscontinuity)", otherTrack->name, otherTrack->playTargetBufferCalc.inSeconds());
 							}
 							break;
 						}
@@ -4344,6 +4354,7 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 					ts->fragmentURI = ts->GetNextFragmentUriFromPlaylist(reloadUri, true);
 					ts->playTarget = ts->playlistPosition;
 					ts->playTargetBufferCalc = ts->playTarget;
+					AAMPLOG_MIL("DJH %s->playTargetBufferCalc set to %f", ts->name, ts->playTargetBufferCalc.inSeconds());
 				}
 
 				// To avoid audio loss while seeking HLS/TS AV of different duration w/o affecting VOD Discontinuities
@@ -4414,6 +4425,8 @@ AAMPStatusType StreamAbstractionAAMP_HLS::Init(TuneType tuneType)
 			if (aamp->culledSeconds > 0)
 			{
 				trackState[iTrack]->playTargetBufferCalc = aamp->culledSeconds + seekPosition;
+				AAMPLOG_MIL("DJH trackState[%d]->playTargetBufferCalc set to %f (culledSeconds %f + seekPosition %f)", 
+						iTrack, trackState[iTrack]->playTargetBufferCalc.inSeconds(), aamp->culledSeconds, seekPosition.inSeconds());
 			}
 		}
 
@@ -4515,6 +4528,7 @@ void StreamAbstractionAAMP_HLS::InitTracks()
 		ts->playlistPosition = -1;
 		ts->playTarget = seekPosition;
 		ts->playTargetBufferCalc = seekPosition;
+		AAMPLOG_MIL("DJH %s->playTargetBufferCalc set to %f", ts->name, ts->playTargetBufferCalc.inSeconds());
 		if (iTrack == eTRACK_SUBTITLE && !aamp->IsSubtitleEnabled())
 		{
 			AAMPLOG_INFO("StreamAbstractionAAMP_HLS::subtitles disabled by application");
@@ -4665,6 +4679,8 @@ double StreamAbstractionAAMP_HLS::GetBufferedDuration()
  */
 double TrackState::GetBufferedDuration()
 {
+	AAMPLOG_ERR("DJH playTargetBufferCalc %f, GetPositionMs %lld",
+			playTargetBufferCalc.inSeconds(), aamp->GetPositionMs());
 	return (playTargetBufferCalc.inSeconds() - (aamp->GetPositionMs() / 1000));
 }
 
@@ -7074,6 +7090,7 @@ void TrackState::SwitchAudioTrack()
 		playlistPosition += fragmentDurationSeconds;
 		playTarget = playlistPosition;
 		playTargetBufferCalc = playTarget;
+		AAMPLOG_MIL("DJH playTargetBufferCalc set to %f", playTargetBufferCalc.inSeconds());
 		//PlayTargetOffset is determined at Init, hence keep it un-reset.
 		//playTargetOffset = 0;
 		AAMPLOG_INFO("Calculated diffInFetchDuration %lf diffInInjectedDuration %lf  LastInjectedFragmentPosition() %lf", diffInFetchedDuration, diffInInjectedDuration, GetLastInjectedFragmentPosition());
