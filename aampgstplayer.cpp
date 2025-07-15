b264a24f95ae97fb1f20e06bb7f07b60c22e525e
cpc005 <61162093+cpc005@users.noreply.github.com>
2025-07-15 11:15:42 +0100

    More debug

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -405,7 +405,7 @@ AAMPGstPlayer::AAMPGstPlayer(PrivateInstanceAAMP *aamp, id3_callback_t id3Handle
 		this->cbExportYUVFrame = exportFrames;
 		playerInstance->gstCbExportYUVFrame = exportFrames;
 		//std::string debugLevel = GETCONFIGVALUE(eAAMPConfig_GstDebugLevel);
-		std::string debugLevel = "basesink=6,GST_ELEMENT_PADS:6";
+		std::string debugLevel = "2,basesink:6,GST_ELEMENT_PADS:6,rialto*:6";
 		AAMPLOG_WARN("debugLevel %s", debugLevel.c_str());
 		if(!debugLevel.empty())
 		{
