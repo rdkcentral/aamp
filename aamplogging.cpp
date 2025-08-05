4deb309195ff1e8da878a1c8ca1081952574d47c
anjali-syna <206662904+anjali-syna@users.noreply.github.com>
2025-08-05 14:08:47 +0530

    reverting the change as image is not able to play anything
    Signed-off-by: anjali-syna <206662904+anjali-syna@users.noreply.github.com>

--- aamplogging.cpp
+++ aamplogging.cpp
@@ -145,8 +145,8 @@ void logprintf(AAMP_LogLevel logLevelIndex, const char* file, int line, const ch
 						ethanLogLevel = ETHAN_LOG_MILESTONE;
 						break;
 				}
-				printf(format_ptr, args);//anj
-				//vethanlog(ethanLogLevel,NULL,NULL,-1,format_ptr, args);//orig git code
+				//printf(format_ptr, args);//anj
+				vethanlog(ethanLogLevel,NULL,NULL,-1,format_ptr, args);//orig git code
 			}
 			else
 			{
