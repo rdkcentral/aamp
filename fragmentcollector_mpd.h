e82ca80a0e89cd9ace6884c7d8e39b08c21dfcb4
Vinish100 <vinish.balan@gmail.com>
2025-08-05 11:28:04 +0530

    VPLAY-9904: Support PTO for DASH SegmentTimeline format
    Reason for change: Handle PTO in SegmentTimeline. Add PTO offset
    from timelineStart to skipTime to skip to the right fragment. This also
    adjusts for seekMidFragment config. Use a new flag mIsFinalFirstPTS to
    determine if we should update PTS in NotifyFirstVideoPTS which will be
    the firstPTS of fragment and doesn't include PTO. Added L1.
    Test Procedure: Test with streams in VPLAY-10064 and also validate
    with the app as in ticket description
    Risks: Low
    
    Signed-off-by: Vinish100 <vinish.balan@gmail.com>

--- fragmentcollector_mpd.h
+++ fragmentcollector_mpd.h
@@ -1271,6 +1271,7 @@ protected:
 	bool mShortAdOffsetCalc;
 	AampTime mNextPts;					/*For PTS restamping*/
 	std::vector<std::unique_ptr<aamp::AampTrackWorker>> mTrackWorkers;	/**< Track workers for fetching fragments*/
+	bool mIsFinalFirstPTS; /**< Flag to indicate if the first PTS is final or not */
 };
 
 #endif //FRAGMENTCOLLECTOR_MPD_H_
