646e6a1e9994c54aeae40baf9ba2315cffcb9573
Neil <nebutler@nebutlervmrdk1.synamedia.com>
2025-08-11 14:46:32 +0000

    VPLAY-10403 SetSubtitleMute

--- middleware/playerLogManager/PlayerLogManager.cpp
+++ middleware/playerLogManager/PlayerLogManager.cpp
@@ -149,8 +149,7 @@ void logprintf(MW_LogLevel logLevelIndex, const char* file, int line, const char
 					    ethanLogLevel = ETHAN_LOG_MILESTONE;
 					    break;
 			    }
-			     vprintf( format_ptr, args );
-
+			    vethanlog(ethanLogLevel,NULL,NULL,-1,format_ptr, args);
 		    }
 		    else
 		    {
