75315df7570e89ba307976b2745a76b976080328
anjali-syna <206662904+anjali-syna@users.noreply.github.com>
2025-08-13 15:05:46 +0530

    trying MP4 type for subtitle
    Signed-off-by: anjali-syna <206662904+anjali-syna@users.noreply.github.com>

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -10836,9 +10836,12 @@ void StreamAbstractionAAMP_MPD::GetStreamFormat(StreamOutputFormat &primaryOutpu
 		}
 		AAMPLOG_WARN("ANJ: subtitleOutputFormat = %d", subtitleOutputFormat);
 #if 1//anj
-		AAMPLOG_WARN("ANJ: If:If:if: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
-		subtitleOutputFormat = FORMAT_SUBTITLE_TTML;
-		AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
+		AAMPLOG_WARN("ANJ: If:If:if: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_MP4 ");
+		subtitleOutputFormat = FORMAT_SUBTITLE_MP4;
+		AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_MP4 ");
+		//AAMPLOG_WARN("ANJ: If:If:if: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
+		//subtitleOutputFormat = FORMAT_SUBTITLE_TTML;
+		//AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
 #endif//anj
 	}
 	else
@@ -10846,9 +10849,12 @@ void StreamAbstractionAAMP_MPD::GetStreamFormat(StreamOutputFormat &primaryOutpu
 		subtitleOutputFormat = FORMAT_INVALID;
 		AAMPLOG_WARN("ANJ: 4. else case: subtitleOutputFormat = FORMAT_INVALID. subtitleOutputFormat = %d", subtitleOutputFormat);
 #if 1//anj
-		AAMPLOG_WARN("ANJ: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
-		subtitleOutputFormat = FORMAT_SUBTITLE_TTML;
-		AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
+		AAMPLOG_WARN("ANJ: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_MP4 ");
+		subtitleOutputFormat = FORMAT_SUBTITLE_MP4;
+		AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_MP4 ");
+		//AAMPLOG_WARN("ANJ: Overwriting subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
+		//subtitleOutputFormat = FORMAT_SUBTITLE_TTML;
+		//AAMPLOG_WARN("ANJ: Overwritten subtitleOutputFormat = FORMAT_SUBTITLE_TTML ");
 #endif//anj
 	}
 }
