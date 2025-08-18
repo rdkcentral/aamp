b1315aeab6337f90909e7b9c76190b2268f2d97f
aahmed878 <Alsameema_Ahmedansar@comcast.com>
2025-08-18 18:10:50 +0000

    RDKEMW-6670  :  Ignore  DRM error for fake tune * Ignore error for fake tune
    * RDKEMW-6670 : updated log prints
    
    Reason for change:
    
    Test Procedure:
    
    Priority: P2
    
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    ---------
    
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Co-authored-by: Kadungoth, Vinod (Contractor) <Vinod_Kadungoth2@comcast.com>
    Co-authored-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    Signed-off-by: aahmed878 <Alsameema_Ahmedansar@comcast.com>

--- externals/rdk/PlayerIarmRdkInterface.cpp
+++ externals/rdk/PlayerIarmRdkInterface.cpp
@@ -78,6 +78,7 @@ void powerModeChangeHandler(const char *owner, IARM_EventId_t eventId, void *dat
                 param->data.state.curState, param->data.state.newState);
         if(param->data.state.curState == IARM_BUS_PWRMGR_POWERSTATE_STANDBY_DEEP_SLEEP && param->data.state.newState != IARM_BUS_PWRMGR_POWERSTATE_STANDBY_DEEP_SLEEP )
         {
+	    printf(" DEEPSLEEP : calling triggerFakeTune  \n");
             triggerFakeTune();
         }
     }
--- main_aamp.cpp
+++ main_aamp.cpp
@@ -56,7 +56,7 @@ void doFakeTune()
 {
 	if(isDevicePropertiesPresent())
 	{
-			AAMPLOG_WARN("Triggering fake tune to wake up AAMP from deep sleep");
+			AAMPLOG_WARN("doFakeTune : Triggering fake tune");
 			std::shared_ptr<PlayerInstanceAAMP> fakeTuneInstance = std::make_shared<PlayerInstanceAAMP>(nullptr, nullptr);
 			std::string jsonStr = R"({
 		    "preferredDrm": 1,
