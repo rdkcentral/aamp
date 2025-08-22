a0b1b0df7dfd33b8632b2e13a215b8b846e9cb48
Alsameema <alsameema_ahmedansar@comcast.com>
2025-08-22 23:16:50 +0530

    RDKEMW-7375: Add Compilation MACRO with specific to PreInit Decoding on RDKE
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -2701,11 +2701,13 @@ void PrivateInstanceAAMP::SendTuneMetricsEvent(std::string &timeMetricData)
  */
 void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char * description, bool isRetryEnabled, int32_t secManagerClassCode, int32_t secManagerReasonCode, int32_t secClientBusinessStatus, const std::string &responseData)
 {
+#ifdef USE_PREINIT_DECODING
 	if(mManifestUrl == FAKE_TUNE_URL)
 	{
 		AAMPLOG_WARN("PrivateInstanceAAMP: Ignore error for fake tune");
 		return;
 	}
+#endif
 	bool sendErrorEvent = false;
 	std::unique_lock<std::recursive_mutex> lock(mLock);
 	if(mState != eSTATE_ERROR)
