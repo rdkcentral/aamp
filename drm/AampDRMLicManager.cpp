45c8fe92ba0d0a6508a69775e4049ad71237d09e
Anurag Krishnan <akrish513@cable.comcast.com>
2025-08-26 21:23:53 +0530

    VPLAY-10820: Observing Technical fault after waking from Deepsleep
    Reason for change: Maintained the single secmanager session in
    middleware component to resolve the closesession failure - feature/VPLAY-10820_2.6.1
    Test Procedure: Refer Jira
    Risks: NA
    
    Signed-off-by: Anurag Krishnan <akrish513@cable.comcast.com>

--- drm/AampDRMLicManager.cpp
+++ drm/AampDRMLicManager.cpp
@@ -1161,11 +1161,11 @@ DrmData * AampDRMLicenseManager::getLicenseSec(const LicenseRequest &licenseRequ
 		int32_t reasonCode;
 		int32_t businessStatus;
 		bool videoMuteState = mDrmSessionManager->mIsVideoOnMute.load();
-		if (!mAampSecManagerSession.isSessionValid())
+		if (!mDrmSessionManager->mAampSecManagerSession.isSessionValid())
 		{
 			// if we're about to get a licence and are not re-using a session, then we have not seen the first video frame yet. Do not allow watermarking to get enabled yet.
 			AAMPLOG_WARN("First frame flag cleared before AcquireLicense, with mIsVideoOnMute=%d", videoMuteState);
-			mFirstFrameSeen.store(false);
+			mDrmSessionManager->mFirstFrameSeen.store(false);
 		}
 
 		tStartTime = NOW_STEADY_TS_MS;
@@ -1177,7 +1177,7 @@ DrmData * AampDRMLicenseManager::getLicenseSec(const LicenseRequest &licenseRequ
 																 keySystem,
 																 mediaUsage,
 																 secclientSessionToken, challengeInfo.accessToken.length(),
-																 mAampSecManagerSession,
+																 mDrmSessionManager->mAampSecManagerSession,
 																 &licenseResponseStr, &licenseResponseLength,
 																 &statusCode, &reasonCode, &businessStatus, videoMuteState);
 		tEndTime = NOW_STEADY_TS_MS;
