ef59a7a47e8d31240fe4a593d53d5e0fcd15e863
Alsameema <alsameema_ahmedansar@comcast.com>
2025-09-15 15:58:19 +0530

    RDKEMW-8209: Fake tune integrated with IARM
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-7375: Address Compilation Failure with ABRManager
    
    Signed-off-by: aahmed878 <Alsameema_Ahmedansar@comcast.com>
    
    RDKEMW-6670 : Tune time improvement with fake tune
    
    * Fake tune integrated with IARM
    
    * RDKEMW-6670 : avoid power handler registration for non jsbinding usage
    
    Reason for change:
    
    Test Procedure:
    
    Priority: P2
    
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    * Faketune with file://
    
    * Updated manifest name to align with local hosted manifest
    
    * Delay changed to 6 seconds
    
    * RDKEMW-6670  : fixing issue in two fake tune prev logic
    
    Reason for change:
    
    Test Procedure:
    
    Priority: P2
    
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    * RDKEMW-6670 : Moved stop to thread
    
    Reason for change:
    
    Test Procedure:
    
    Priority: P2
    
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    * RDKEMW-6670 : sleep 7 seconds
    
    * RDKEMW-6670: Modify file location from /opt to /etc
    
    Signed-off-by: aahmed878 <Alsameema_Ahmedansar@comcast.com>
    
    ---------
    
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Signed-off-by: aahmed878 <Alsameema_Ahmedansar@comcast.com>
    Co-authored-by: Kadungoth, Vinod (Contractor) <Vinod_Kadungoth2@comcast.com>
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-6670  :  Ignore  DRM error for fake tune
    
    * Ignore error for fake tune
    
    * RDKEMW-6670 : updated log prints
    
    Reason for change:
    
    Test Procedure:
    
    Priority: P2
    
    Risks:Low
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    
    ---------
    
    Signed-off-by: shripad bankar <Shripad_Bankar@cable.comcast.com>
    Co-authored-by: Kadungoth, Vinod (Contractor) <Vinod_Kadungoth2@comcast.com>
    
    RDKEMW-7175: Avoid License Acquisition call for FakeTune
    
    Signed-off-by: aahmed878 <Alsameema_Ahmedansar@comcast.com>
    
    RDKEMW-7375: Add Compilation MACRO with specific to RDKE
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-7375: Add Compilation MACRO with specific to PreInit Decoding on RDKE
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-7375: Rename Compilation MACRO with specific to PreInit Decoding on RDKE
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-7375: Add Compilation MACRO for Libaamp, JS, FBInterface libs
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>
    
    RDKEMW-8209: Fixed FAKE_TUNE Macro Compilation Error
    
    Signed-off-by: Alsameema <alsameema_ahmedansar@comcast.com>

--- middleware/externals/PlayerExternalsInterfaceBase.h
+++ middleware/externals/PlayerExternalsInterfaceBase.h
@@ -102,7 +102,7 @@ class PlayerExternalsInterfaceBase
          * @brief Initialize IARM
          * @param[in] processName string of the name of the process initializing IARM
          */
-        static void IARMInit(const char* processName){}
+        static void IARMInit(const char* processName, bool powerEvt){}
 
         /**
          * @fn IARMRegisterDsMgrEventHandler
