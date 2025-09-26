3fbef1860773431745caf8468b7c928167a514bf
deepika <deepikasri_n@comcast.com>
2025-09-26 09:30:25 +0530

    compilaton issues

--- middleware/InterfacePlayerRDK.cpp
+++ middleware/InterfacePlayerRDK.cpp
@@ -632,7 +632,7 @@ void MonitorAV( InterfacePlayerRDK *pInterfacePlayerRDK )
 			{ // log only when interpretation of AV state has changed
 				if( monitorAVState->description )
 				{ // avoid logging for initial NULL description
-					MW_LOG_MIL( "MonitorAV_%s: %" G_GINT64_FORMAT ",%" G_GINT64_FORMAT ",%d,%lld",
+					MW_LOG_MIL( "MonitorAV_%s: %" G_GINT64_FORMAT ",%" G_GINT64_FORMAT ",%d,%"G_GINT64_FORMAT,
 							   monitorAVState->description,
 							   monitorAVState->av_position[eGST_MEDIATYPE_VIDEO],
 							   monitorAVState->av_position[eGST_MEDIATYPE_AUDIO],
