191d3dd3be2f8c49f30792ba96debf9dd4d6544d
haripriya_molakalapalli <haripriya_molakalapalli@comcast.com>
2025-10-21 14:04:09 -0400

    VPLAY-11407 :Migrate AAMP Eventlisteners to shared_ptr for better memory handling
    Reason for change: Sling App crash due to dangling pointer
    Test Procedure: Updated in the ticket
    Risks: Medium
    
    Signed-off-by: haripriya_molakalapalli <haripriya_molakalapalli@comcast.com>

--- main_aamp.h
+++ main_aamp.h
@@ -355,6 +355,15 @@ public:
 	void AddEventListener(AAMPEventType eventType, std::shared_ptr<EventListener> eventListener);
 
 	/**
+	 *   @fn AddEventListener
+	 *
+	 *   @param[in]  eventType - type of event.
+	 *   @param[in]  eventListener - listener for the eventType - raw pointer.
+	 *   @return void
+	 */
+	void AddEventListener(AAMPEventType eventType, EventListener* eventListener);
+
+	/**
 	 *   @fn RemoveEventListener
 	 *
 	 *   @param[in]  eventType - type of event.
