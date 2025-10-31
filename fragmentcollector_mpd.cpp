45a331a245c2a6d4d3ea46000e1871359645e0bc
jfagunde <83724616+jfagunde@users.noreply.github.com>
2025-10-31 14:55:43 +0100

    VPLAY-11594 Remove rate condition from UpdatePtsOffset() (#635)
    VPLAY-11594 Remove rate condition from UpdatePtsOffset()
    
    Reason for Change:  Remove the rate condition from UpdatePtsOffset(). The PTS offset should be updated regardless of the rate. This is important when using AAMP TSB, since downloading segments and calculating the PTS offset is independent from the play rate. However, this issue does not currently impact the behaviour due to VPLAY-11583. When using FOG or cloud TSB, this condition does not matter (the PTS offset is not used for trick modes). 
    
    Test Guidance: General Regression only + updated L1 tests passing.
    
    Risk: Low

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -9340,41 +9340,36 @@ void StreamAbstractionAAMP_MPD::UpdatePtsOffset(bool isNewPeriod)
 	AampTime timelineStart;
 	AampTime duration;
 
-	// Nothing to do during trick play, so skip code
-	if (mPlayRate == AAMP_NORMAL_PLAY_RATE)
+	IPeriod *period = mCurrentPeriod;
+	GetStartAndDurationForPtsRestamping(timelineStart, duration);
+
+	if (isNewPeriod)
 	{
-		IPeriod *period = mCurrentPeriod;
-		GetStartAndDurationForPtsRestamping(timelineStart, duration);
 
-		if (isNewPeriod)
+		if (mShortAdOffsetCalc)
 		{
+			/* This is for the case of a short ad that is not as long as the base period which
+			 * it replaces. The ad has been 'played' and now we need to return and play out the remaining
+			 * segments in the base period
+			 */
+			mShortAdOffsetCalc = false;
+			double audioStart = mMediaStreamContext[eMEDIATYPE_AUDIO]->fragmentDescriptor.Time /
+								mMediaStreamContext[eMEDIATYPE_AUDIO]->fragmentDescriptor.TimeScale;
+			double videoStart = mMediaStreamContext[eMEDIATYPE_VIDEO]->fragmentDescriptor.Time /
+								mMediaStreamContext[eMEDIATYPE_VIDEO]->fragmentDescriptor.TimeScale;
 
-			if (mShortAdOffsetCalc)
-			{
-				/* This is for the case of a short ad that is not as long as the base period which
-				 * it replaces. The ad has been 'played' and now we need to return and play out the remaining
-				 * segments in the base period
-				 */
-				mShortAdOffsetCalc = false;
-				double audioStart = mMediaStreamContext[eMEDIATYPE_AUDIO]->fragmentDescriptor.Time /
-									mMediaStreamContext[eMEDIATYPE_AUDIO]->fragmentDescriptor.TimeScale;
-				double videoStart = mMediaStreamContext[eMEDIATYPE_VIDEO]->fragmentDescriptor.Time /
-									mMediaStreamContext[eMEDIATYPE_VIDEO]->fragmentDescriptor.TimeScale;
+			AampTime newStart = std::max(audioStart, videoStart);
 
-				AampTime newStart = std::max(audioStart, videoStart);
-
-				mNextPts += timelineStart - newStart;
-				AAMPLOG_INFO("newStart %f timelineStart %f", newStart.inSeconds(), timelineStart.inSeconds());
-			}
-			mPTSOffset += mNextPts - timelineStart;
-
-			AAMPLOG_INFO("Idx %d Id %s mPTSOffsetSec %f mNextPts %f timelineStartSec %f",
-						mCurrentPeriodIdx, period->GetId().c_str(), mPTSOffset.inSeconds(), mNextPts.inSeconds(), timelineStart.inSeconds());
+			mNextPts += timelineStart - newStart;
+			AAMPLOG_INFO("newStart %f timelineStart %f", newStart.inSeconds(), timelineStart.inSeconds());
 		}
+		mPTSOffset += mNextPts - timelineStart;
 
-		mNextPts = duration + timelineStart;
-
+		AAMPLOG_INFO("Idx %d Id %s mPTSOffsetSec %f mNextPts %f timelineStartSec %f",
+					mCurrentPeriodIdx, period->GetId().c_str(), mPTSOffset.inSeconds(), mNextPts.inSeconds(), timelineStart.inSeconds());
 	}
+
+	mNextPts = duration + timelineStart;
 }
 
 void StreamAbstractionAAMP_MPD::RestorePtsOffsetCalculation(void)
