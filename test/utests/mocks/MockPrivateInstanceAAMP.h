eacdd25e83ab874aa10286dbd2e243a3d9604de2
anjali-syna <206662904+anjali-syna@users.noreply.github.com>
2025-11-05 08:58:40 +0530

    VPLAY-11328 - Linear - Progress Bar jumping to the beginning while trickplay (#640)
    VPLAY-11328 - [Linear]Progress Bar jumping to the End/beginning while performing Trick Play
    
    Reason for change: Progress Bar Point is jumping to the End/beginning
     while performing Trick Play/Seek in linear services. While analysing
     the pts value of the segments, it is noticed that the restamped pts
     value of some of the segments in the main content is a very high value
     compared to the previous segment. This is happening because a wrong
     timescale is used for pts re calculation. RecalculatePTS function is
     using the timescale value saved in PrivateInstanceAAMP. When the downloads
     progresses from content to ad the timescale of the segment can change.
     When this happens, if the pay rate is not normal play rate, the new timescale
     is not set to PrivateInstanceAAMP and thus causing the issue.
    
    Changes:
    * CacheFragment function is modified to set the timescale to PrivateInstanceAAMP
      irrespective of the play rate.
    
    Test Procedure: Refer JIRA ticket
    
    Risks: low
    
    * Adding new L1 test
    
    Signed-off-by: anjali-syna <206662904+anjali-syna@users.noreply.github.com>

--- test/utests/mocks/MockPrivateInstanceAAMP.h
+++ test/utests/mocks/MockPrivateInstanceAAMP.h
@@ -65,6 +65,7 @@ public:
 	MOCK_METHOD(void, SendAdResolvedEvent, (const std::string &adId, bool status, uint64_t startMS, uint64_t durationMs, AAMPCDAIError errorCode));
 	MOCK_METHOD(uint32_t, GetAudTimeScale, ());
 	MOCK_METHOD(uint32_t, GetVidTimeScale, ());
+	MOCK_METHOD(void, SetVidTimeScale, (uint32_t));
 	MOCK_METHOD(void, ProcessID3Metadata, (char *, size_t , AampMediaType , uint64_t ));
 	MOCK_METHOD(void, SetPauseOnStartPlayback, (bool enable));
 	MOCK_METHOD(bool, isDecryptClearSamplesRequired, ());
