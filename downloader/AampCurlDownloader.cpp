204810d743760a97fd42e98f2e4cf550c38e5730
vkadun208_comcast <vinod_kadungoth2@comcast.com>
2025-11-24 10:22:09 -0500

    Split AampCurlDownloader::Release() to fix thread-safe resource cleanup
    Separated the Release() function into two parts to ensure thread-safe
    cleanup of curl headers:
    
    1. Release() - Disables the downloader by setting mDownloadActive flag,
       allowing download threads to exit gracefully
    2. ReleaseHeaders() - Cleans up curl headers and resets timing variables
       after threads have been joined
    
    This prevents race conditions where headers could be freed while download
    threads are still active and potentially accessing them.

--- downloader/AampCurlDownloader.cpp
+++ downloader/AampCurlDownloader.cpp
@@ -372,6 +372,11 @@ void AampCurlDownloader::Release()
 {
 	std::lock_guard<std::mutex> lock(mCurlMutex);
 	mDownloadActive = false;
+}
+
+void AampCurlDownloader::ReleaseHeaders()
+{
+	std::lock_guard<std::mutex> lock(mCurlMutex);
 	mDownloadUpdatedTime = 0 ;
 	mDownloadStartTime =  0;
 	mWriteCallbackBufferSize = 0;
