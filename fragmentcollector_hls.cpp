a1ea4f3b1c41214844e86f405fa9656f633ff3bb
pstroffolino <Philip_Stroffolino@cable.comcast.com>
2025-12-01 12:16:31 -0500

    VPLAY-11949 godspell cleanup - Nov 2025 (#719)
    VPLAY-11949 godspell cleanup - Nov 2025
    
    Reason for Change: replace prioritizes with priority
    
    Risk: None (change in comment)
    
    Test Guidance: no godspell warnings
    
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -7583,7 +7583,7 @@ void StreamAbstractionAAMP_HLS::SelectSubtitleTrack()
  *
  * Scoring algorithm:
  * - Base: 1 point for any track
- * - Language: (list_size - position) * AAMP_LANGUAGE_SCORE (prioritizes order)
+ * - Language: (list_size - position) * AAMP_LANGUAGE_SCORE (prioritize order)
  * - Rendition: AAMP_ROLE_SCORE
  * - Name: AAMP_TYPE_SCORE
  *
