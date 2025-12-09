54cc7ef0468acb4a438203dce6e8db887026185e
nejuma1 <nejumathopppil_nazar@comcast.com>
2025-12-09 05:55:48 +0000

    Revert "VPLAY-11395 L2 Test AAMP-TUNE-1012_ManifestPreProcess failing (#577)"
    This reverts commit 20cee18e4286c6d786ed2635ee27f98ba3e2669b.

--- test/aampcli/AampcliPlaybackCommand.cpp
+++ test/aampcli/AampcliPlaybackCommand.cpp
@@ -1183,22 +1183,20 @@ char * PlaybackCommand::commandRecommender(const char *text, int state)
 
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp)
 {
-	size_t n = size * nmemb;
-    userp->append( (char*)contents, n );
-    return n;
+    userp->append((char*)contents, size * nmemb);
+    return size * nmemb;
 }
 
 std::string PlaybackCommand::getManifestData(std::string& url)
 {
+	CURL* curl;
 	std::string manifestData;
 	if( url.substr(0, 7) == "http://" || url.substr(0, 8) == "https://" )
 	{
 		auto delim = url.find('@');
-		CURL* curl = curl_easy_init();
+		curl = curl_easy_init();
 		if(curl)
 		{
-			CURL_EASY_SETOPT_STRING(curl, CURLOPT_USERAGENT, "aamp-getManifestData/1.0");
-			
 			if( delim != std::string::npos )
 			{
 				std::string range = url.substr(delim+1);
@@ -1214,12 +1212,9 @@ std::string PlaybackCommand::getManifestData(std::string& url)
 			(void)curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 			(void)curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 			(void)curl_easy_setopt(curl, CURLOPT_WRITEDATA, &manifestData);
+			(void)curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
 			CURLcode rc = curl_easy_perform(curl);
-			if (CURLE_OK != rc)
-			{
-				AAMPCLI_PRINTF("[AAMPCLI] %s curl error: %u", __FUNCTION__, rc);
-			}
-			else
+			if (CURLE_OK == rc)
 			{
 				long response_code = 0;
 				(void)curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
@@ -1230,7 +1225,8 @@ std::string PlaybackCommand::getManifestData(std::string& url)
 					case 206:
 						break;
 					default:
-						AAMPCLI_PRINTF("[AAMPCLI] %s http error: %ld", __FUNCTION__,response_code);
+						// http error
+						AAMPCLI_PRINTF("[AAMPCLI] %s curl error code : %ld",__FUNCTION__,response_code);
 						break;
 				}
 			}
