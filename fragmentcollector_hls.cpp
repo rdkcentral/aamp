959c7fb6b39e573fd16823bd06d867841dd5efc8
DomSyna <192202460+DomSyna@users.noreply.github.com>
2025-12-09 09:08:40 +0000

    Add sub-type information to best text track log

--- fragmentcollector_hls.cpp
+++ fragmentcollector_hls.cpp
@@ -7672,9 +7672,9 @@ bool StreamAbstractionAAMP_HLS::SelectPreferredTextTrack(TextTrackInfo &selected
 			bestScore = score;
 			selectedTextTrack = track;
 
-			AAMPLOG_INFO("New best text track: lang=%s, rendition=%s, name=%s, CC=%s, score=%llu",
+			AAMPLOG_INFO("New best text track: lang=%s, rendition=%s, name=%s, CC=%s, score=%llu, sub-type=%s",
 						 track.language.c_str(), track.rendition.c_str(),
-						 track.name.c_str(), track.isCC ? "True" : "False", score);
+						 track.name.c_str(), track.isCC ? "True" : "False", score, track.isCC ? "CLOSED-CAPTIONS" : "SUBTITLES");
 		}
 	}
 
