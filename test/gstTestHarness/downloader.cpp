d2ea6878d267c3900b137cf182a25df0bcedd53c
Philip Stroffolino <philip_stroffolino@cable.comcast.com>
2025-12-24 14:15:46 -0500

    Reason for Change: moved user agent set to be only done for http and https downloads
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- test/gstTestHarness/downloader.cpp
+++ test/gstTestHarness/downloader.cpp
@@ -130,7 +130,6 @@ gpointer LoadUrl( const std::string &url, gsize *pLen )
 		delim++;
 	}
 	
-	curl_easy_setopt(context.curl, CURLOPT_USERAGENT, "gstTestHarness/1.0");
 	if( starts_with(url,"http://") || starts_with(url,"https://" ) )
 	{
 		if( range.empty() )
@@ -149,7 +148,8 @@ gpointer LoadUrl( const std::string &url, gsize *pLen )
 		(void)curl_easy_setopt(context.curl, CURLOPT_WRITEFUNCTION, CurlContext::write_callback);
 		(void)curl_easy_setopt(context.curl, CURLOPT_WRITEDATA, &context);
 		(void)curl_easy_setopt(context.curl, CURLOPT_TCP_KEEPALIVE, 1L);
-		
+		(void)curl_easy_setopt(context.curl, CURLOPT_USERAGENT, "gstTestHarness/1.0");
+
 		context.clear();
 		CURLcode rc = curl_easy_perform(context.curl);
 		if (CURLE_OK == rc)
