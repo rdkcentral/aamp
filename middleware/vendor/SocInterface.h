b9c3e2793f71bd5111b36d3a76332b40f8b14e4a
pstroffolino <Philip_Stroffolino@cable.comcast.com>
2026-01-14 12:35:36 -0500

    Revert "RDK-60157 : unified support for SetAC4Tracks (#836)" (#856)
    This reverts commit 9a7b8241bf0e6a6b7a5183ae3f76d49c14213e65.

--- middleware/vendor/SocInterface.h
+++ middleware/vendor/SocInterface.h
@@ -277,7 +277,7 @@ public:
 	 * @param src Source element.
 	 * @param trackId Track ID.
 	 */
-	virtual void SetAC4Tracks(GstElement *src, int trackId);
+	virtual void SetAC4Tracks(GstElement *src, int trackId) = 0;
 	
 	/**
 	 * @brief Set platform playback rate.
