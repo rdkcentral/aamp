ad914bd7a7af1125b22f2fffff65cd7dc2f4c45e
DomSyna <192202460+DomSyna@users.noreply.github.com>
2026-01-21 10:02:30 +0000

    RDKEMW-11912 Prevent AAMP from FFWD when at live

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -689,7 +689,6 @@ void PlayerInstanceAAMP::SetRateInternal(float rate,int overshootcorrection)
 				aamp->NotifySpeedChanged(AAMP_NORMAL_PLAY_RATE); // Send speed change event to XRE to reset the speed to normal play since the trickplay ignored at player level.
 				return;
 			}
-
 			// Special case where playback has not started due to autoplay being false and
 			// first rate is paused, set to pause with first frame shown
 			if ((AAMP_RATE_PAUSE == rate) && aamp->pipeline_paused && !aamp->mbPlayEnabled && !aamp->mbDetached)
@@ -729,9 +728,12 @@ void PlayerInstanceAAMP::SetRateInternal(float rate,int overshootcorrection)
 				}
 			}
 			bool retValue = true;
-			if ( AAMP_SLOWMOTION_RATE != rate && rate > 0 && aamp->IsLive() && aamp->mpStreamAbstractionAAMP->IsStreamerAtLivePoint() && aamp->rate >= AAMP_NORMAL_PLAY_RATE && !aamp->mbDetached)
+
+			if (rate >= AAMP_NORMAL_PLAY_RATE &&
+				aamp->IsAtLivePoint() &&
+				!aamp->mbDetached)
 			{
-				AAMPLOG_WARN("Already at logical live point, hence skipping operation");
+				AAMPLOG_WARN("Already at logical live point, hence skipping operation (rate=%.2f)", rate);
 				aamp->NotifyOnEnteringLive();
 				return;
 			}
@@ -743,6 +745,9 @@ void PlayerInstanceAAMP::SetRateInternal(float rate,int overshootcorrection)
 				AAMPLOG_WARN("Already running at playback rate(%f) pipeline_paused(%d), hence skipping set rate for (%f)", aamp->rate, aamp->pipeline_paused, rate);
 				return;
 			}
+
+			// Any other rate change (including resume to 1.0) = not at live
+			aamp->mpStreamAbstractionAAMP->IsStreamerAtLivePoint(); 
 
 			//-- Get the trick play to a closer position
 			//Logic adapted
