03d679025db6b428f72fab2a5cd0b27d09631b35
Philip Stroffolino <philip_stroffolino@cable.comcast.com>
2026-01-22 11:27:25 -0500

    Reason for Change: removed pair of const keywords (breaking osx compilation)
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- MediaStreamContext.cpp
+++ MediaStreamContext.cpp
@@ -957,14 +957,14 @@ bool MediaStreamContext::DownloadFragment(DownloadInfoPtr dlInfo)
 			mActiveDownloadInfo = dlInfo;
 		}
 		int maxCachedFragmentsPerTrack = GETCONFIGVALUE(eAAMPConfig_MaxFragmentCached); // Max cached fragments per track
-		auto DownloadsEnabled = [this]() const
+		auto DownloadsEnabled = [this]()
 		{
 			return aamp->DownloadsAreEnabled() && !abort;
 		};
 		// In low-latency mode, wait for needData/enoughData signals before
 		// downloading the next fragment. Skip this wait when injecting from
 		// the local AAMP TSB.
-		auto WaitForLowLatencyDashDownloads = [this, DownloadsEnabled]() const
+		auto WaitForLowLatencyDashDownloads = [this, DownloadsEnabled]()
 		{
 			return DownloadsEnabled() &&
 				   !aamp->IsLocalAAMPTsbInjection() &&
