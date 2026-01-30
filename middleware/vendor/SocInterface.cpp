3efd5f4a3e39784c0e2b47a7343df0874c27e119
nejuma1 <nejumatn28@gmail.com>
2026-01-30 13:07:59 +0530

    Update SocInterface.cpp

--- middleware/vendor/SocInterface.cpp
+++ middleware/vendor/SocInterface.cpp
@@ -256,7 +256,7 @@ void SocInterface::ConfigureAcceptCaps(GstBaseTransformClass* base_transform_cla
  */
 void SocInterface::SetAC4Tracks(GstElement *src, int trackId)
 {
-	MW_LOG_INFO("Selecting AC4 Track Id : %d", trackId);
+	MW_LOG_ERR("Selecting AC4 Track Id : %d", trackId);
 	if(src)
 	{
 		g_object_set(src, "ac4-presentation-group-index", trackId, NULL);
