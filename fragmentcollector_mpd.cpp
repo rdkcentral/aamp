0fe8a849edbf2d29a53764694df7fa60839e9373
James Lofthouse <james_lofthouse@comcast.com>
2026-02-02 18:45:36 +0000

    Crash in GetCurrentMimeType() on empty period Reason for Change: Potential floating pointer with empty periods Test Procedure: Tune regression testing Risk: Low
    Signed-off-by: James Lofthouse <james_lofthouse@comcast.com>

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -7496,6 +7496,7 @@ AAMPStatusType StreamAbstractionAAMP_MPD::UpdateTrackInfo(bool modifyDefaultBW, 
 			{
 				AAMPLOG_WARN("empty period");
 				pMediaStreamContext->adaptationSet = NULL;
+				pMediaStreamContext->representation = NULL;
 				continue;
 			}
 			if (pMediaStreamContext->adaptationSetIdx >= numAdaptationSets )
