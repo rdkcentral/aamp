07d872a3c7077b4831154da3e1956e7cf2bbb6f7
Gnanesha <gnaani82@gmail.com>
2026-02-03 12:59:49 -0500

    VPLAY-12061:Closed captioning issues after introducing webvTT along with inBand CC tracks (#958)
    Reason for change: filter out the webvtt tracks before sending the list of tracks to application
    Test Procedure: see ticket
    Risks: Low
    
    Signed-off-by: Gnanesha <gnaani82@gmail.com>

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -367,7 +367,8 @@ static const ConfigLookupEntryBool mConfigLookupTableBool[AAMPCONFIG_BOOL_COUNT]
 	{false, "useMp4Demux", eAAMPConfig_UseMp4Demux,false },
 	{false, "curlThroughput", eAAMPConfig_CurlThroughput, false },
 	{false, "useFireboltSDK", eAAMPConfig_UseFireboltSDK, false},
-	{true, "enableChunkInjection", eAAMPConfig_EnableChunkInjection, true}
+	{true, "enableChunkInjection", eAAMPConfig_EnableChunkInjection, true},
+	{false, "disableWebVTT", eAAMPConfig_DisableWebVTT, false},
 };
 
 #define CONFIG_INT_ALIAS_COUNT 2
