b0b8b037dff2ff874a9e99c5daa28dfcf5779237
haripriya_molakalapalli <haripriya_molakalapalli@comcast.com>
2026-02-09 14:58:07 -0500

    VPLAY-11433 : Enhancement Request: Utilize Existing Player Source Field in PCTune_split to Indicate VIPA vs Non-VIPA Playback
    Reason for Change :  To better collect and analyze tune metrics,
    use the existing player source string field to differentiate VIPA Playback
    
    Test Procedure: Tune to linear/VOD content , verify Tune log
    Priority : p1
    Risks: Low
    
    Signed-off-by: haripriya_molakalapalli <haripriya_molakalapalli@comcast.com>

--- AampProfiler.cpp
+++ AampProfiler.cpp
@@ -274,7 +274,7 @@ void ProfileEventAAMP::TuneEnd(TuneEndMetrics &mTuneEndMetrics,std::string appNa
 	}
 	enabled = false;
 	unsigned int licenseAcqNWTime = bucketDuration(PROFILE_BUCKET_LA_NETWORK);
-	char tuneTimeStrPrefix[64];
+	char tuneTimeStrPrefix[128];
 	memset(tuneTimeStrPrefix, '\0', sizeof(tuneTimeStrPrefix));
 	int mTotalTime;
  	int mTimedMetadataStartTime = static_cast<int> (mTuneEndMetrics.mTimedMetadataStartTime - tuneStartMonotonicBase);
@@ -297,6 +297,9 @@ void ProfileEventAAMP::TuneEnd(TuneEndMetrics &mTuneEndMetrics,std::string appNa
 	}
 	if (!appName.empty())
 	{
+		if (gpGlobalConfig->IsConfigSet(eAAMPConfig_UseFireboltSDK)) {
+			appName += "_VIPA";
+		}
 		snprintf(tuneTimeStrPrefix, sizeof(tuneTimeStrPrefix), "%s PLAYER[%d] APP: %s IP_AAMP_TUNETIME", playerActiveMode.c_str(),playerId,appName.c_str());
 	}
 	else
