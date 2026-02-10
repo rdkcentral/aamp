c1715ec3e93eaa6655a072b80a59be44a47a5ac3
Vinish100 <vinish.balan@gmail.com>
2026-02-10 12:16:48 +0530

    VPLAY-12548: Factor chunk duration in buffer calculation
    Reason for change: Update lastDownloadedDuration with
    the chunk duration for improved accuracy of buffered
    duration. Chunk duration in ticks are updated when
    chunk boundaries are identified by downloader
    Test Procedure: Check buffer values are updated
    in between chunk downloads with LLD
    Risks: Low
    
    Signed-off-by: Vinish100 <vinish.balan@gmail.com>

--- test/utests/fakes/FakeIsoBmffBuffer.cpp
+++ test/utests/fakes/FakeIsoBmffBuffer.cpp
@@ -162,11 +162,11 @@ int IsoBmffBuffer::UpdateBufferData(size_t parsedBoxCount, char *&unParsedBuffer
 	}
 }
 
-double IsoBmffBuffer::getTotalChunkDuration(int lastMDatIndex)
+uint64_t IsoBmffBuffer::getTotalChunkDurationInTicks(int lastMDatIndex)
 {
 	if (g_mockIsoBmffBuffer)
 	{
-		return g_mockIsoBmffBuffer->getTotalChunkDuration(lastMDatIndex);
+		return g_mockIsoBmffBuffer->getTotalChunkDurationInTicks(lastMDatIndex);
 	}
 	else
 	{
@@ -290,5 +290,17 @@ bool IsoBmffBuffer::getChunkedMdatBoxInfo(size_t &start, size_t &size) const
     else
     {
         return false;
+    }
+}
+
+int IsoBmffBuffer::getLastMdatBoxIndex() const
+{
+    if (g_mockIsoBmffBuffer)
+    {
+        return g_mockIsoBmffBuffer->getLastMdatBoxIndex();
+    }
+    else
+    {
+        return -1;
     }
 }
