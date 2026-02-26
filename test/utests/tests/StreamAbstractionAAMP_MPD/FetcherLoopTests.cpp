48a1a4ce03160027bdc37b3374868035f77c9373
varshnie <varshniblue14@gmail.com>
2026-02-26 20:17:15 +0530

    VPLAY-12744:Handle static DASH DRM license rotation using EAP
    Reason for change:Adding the support to detect EarlyAvailablePeriod to Queue Content Protection
    Risks: Medium
    
    Signed-off-by: varshnie <varshniblue14@gmail.com>

--- test/utests/tests/StreamAbstractionAAMP_MPD/FetcherLoopTests.cpp
+++ test/utests/tests/StreamAbstractionAAMP_MPD/FetcherLoopTests.cpp
@@ -271,6 +271,7 @@ protected:
 			{eAAMPConfig_useRialtoSink, false},
 			{eAAMPConfig_InterruptHandling, false},
 			{eAAMPConfig_UseMp4Demux, false},
+			{eAAMPConfig_ProcessLicenseFromEAP, false},
 };
 
 	BoolConfigSettings mBoolConfigSettings;
