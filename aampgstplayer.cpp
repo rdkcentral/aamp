67645f1c2b43d5b29b8fd66cb6fa70058b0c5a79
dp0000 <53818367+dp0000@users.noreply.github.com>
2026-02-26 12:50:43 +0530

    Update aampgstplayer.cpp

--- aampgstplayer.cpp
+++ aampgstplayer.cpp
@@ -395,8 +395,7 @@ AAMPGstPlayer::AAMPGstPlayer(PrivateInstanceAAMP *aamp, id3_callback_t id3Handle
 
 {
 	privateContext = new AAMPGstPlayerPriv();
-	playerInstance = new InterfacePlayerRDK(ISCONFIGSET(eAAMPConfig_useRialtoSink));                                       // for time being to use across class and non-class members when progressive testing
-//	playerInstance = new InterfacePlayerRDK(true);                                       // for time being to use across class and non-class members when progressive testing
+	playerInstance = new InterfacePlayerRDK(ISCONFIGSET(eAAMPConfig_useRialtoSink));                                       // for time being to use across class and non-class members when progressive testing                                     // for time being to use across class and non-class members when progressive testing
 	RegisterBusCb(this, playerInstance);
 	if(privateContext)
 	{
