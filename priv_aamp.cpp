a86405b40f515fbcc0b547ac88407f2bf2d264a3
Jose Fagundez <jfagunde@synamedia.com>
2026-03-20 16:16:03 +0000

    VPLAY-12917 Only set subtitles_muted in TuneHelper() for in-band CC
    Reason for Change: L3 test 2003 failure
    
    Summary of Changes:
    * Modified TuneHelper() to only set subtitles_muted for in-band CC
    
    Test Procedure: L3 test 2003
    
    Risks: Low

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -5843,6 +5843,7 @@ static int aampApplyThreadPrioFromEnv(const char *env, int defaultPolicy, int de
 void PrivateInstanceAAMP::TuneHelper(TuneType tuneType, bool seekWhilePaused)
 {
 	bool newTune;
+	bool previousCCEnabled = false;
 
 	aampApplyThreadPrioFromEnv("AAMP_AV_PIPELINE_PRIORITY", SCHED_OTHER, 0);
 	for (int i = 0; i < AAMP_TRACK_COUNT; i++)
@@ -6291,8 +6292,8 @@ void PrivateInstanceAAMP::TuneHelper(TuneType tuneType, bool seekWhilePaused)
 		}
 
 		// Retrieve the current closed‑captioning state and log it along with the in‑band CC flag.
-		subtitles_muted = !PlayerCCManager::GetInstance()->GetStatus();
-		AAMPLOG_WARN("SubtitlesMuted:%d isCCinBand:%d", subtitles_muted.load(), mIsInbandCC);
+		previousCCEnabled = PlayerCCManager::GetInstance()->GetStatus();
+		AAMPLOG_MIL("previousCCEnabled:%d isCCinBand:%d", previousCCEnabled, mIsInbandCC);
 
 		if (!mbUsingExternalPlayer)
 		{
@@ -6422,7 +6423,9 @@ void PrivateInstanceAAMP::TuneHelper(TuneType tuneType, bool seekWhilePaused)
 		//restore CC if it was enabled for previous content.
 		if(mIsInbandCC)
 		{
-			PlayerCCManager::GetInstance()->RestoreCC(!subtitles_muted.load());
+			// Set subtitles_muted so that AAMP will not change the CC state when SetCCStatusInternal() is called.
+			subtitles_muted = !previousCCEnabled;
+			PlayerCCManager::GetInstance()->RestoreCC(previousCCEnabled);
 		}
 	}
 
