2dfc4fddeba6b845924bc7e2e62d18d8017cfb0a
pstroffolino <Philip_Stroffolino@cable.comcast.com>
2026-03-23 11:50:09 -0400

    VPLAY-13097 "User Agent:" dead code removal (#1218)
    Reason for Change: remove AAMP_USER_AGENT_MAX_CONFIG_LEN definition, unused anywhere in code
    
    Test Guidance: no compilation issue
    
    Risk: None
    
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- priv_aamp.h
+++ priv_aamp.h
@@ -119,7 +119,6 @@ namespace aamp
 #define MANIFEST_TIMEOUT_FOR_LLD 3      /**< 3 sec timeout for manifest refresh in case of LLD*/
 #define ABR_BUFFER_COUNTER_FOR_LLD 3		/** Counter for steady state rampup/rampdown for lld */
 
-#define AAMP_USER_AGENT_MAX_CONFIG_LEN  512    /**< Max Chars allowed in aamp.cfg for user-agent */
 #define SERVER_UTCTIME_DIRECT "urn:mpeg:dash:utc:direct:2014"
 #define SERVER_UTCTIME_HTTP "urn:mpeg:dash:utc:http-xsdate:2014"
 // MSO-specific VSS Service Zone identifier in URL
