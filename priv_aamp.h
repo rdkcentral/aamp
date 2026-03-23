0f625847daa3082380ae5b640a9f9f45e9513aac
james_lofthouse <33629205+jameslofthouse-RED@users.noreply.github.com>
2026-03-23 13:23:21 +0000

    VPLAY-13070 Reduce repetitive logging (#1209)
    VPLAY-13070 Reduce repetitive logging
    Reason for change: Change aamp ABR code to log less often (only on changes)
    Test Procedure: See ticket
    Risks: low
    
    ---------
    
    Signed-off-by: James Lofthouse <james_lofthouse@comcast.com>
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>
    Co-authored-by: pstroffolino <Philip_Stroffolino@cable.comcast.com>

--- priv_aamp.h
+++ priv_aamp.h
@@ -1315,11 +1315,11 @@ public:
 	/**
 	 * @fn SetCurlTimeout
 	 *
-	 * @param[in] timeout - maximum time  in seconds curl request is allowed to take
+	 * @param[in] timeoutMs - maximum time in milliseconds curl request is allowed to take
 	 * @param[in] instance - index of curl instance to which timeout to be set
-	 * @return void
+	 * @return true if timeout changed, else false
 	 */
-	void SetCurlTimeout(long timeout, AampCurlInstance instance);
+	bool SetCurlTimeout(long timeoutMs, AampCurlInstance instance);
 
 	/**
 	 * @brief Set manifest curl timeout
