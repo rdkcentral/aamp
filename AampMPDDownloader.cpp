46e7caaebe602032619b623f514411e5b2de3a1d
pstroffolino <Philip_Stroffolino@cable.comcast.com>
2026-03-27 21:36:46 -0400

    Update AampMPDDownloader.cpp

--- AampMPDDownloader.cpp
+++ AampMPDDownloader.cpp
@@ -399,7 +399,6 @@ void AampMPDDownloader::downloadMPDThread1()
 				mDownloader1.Download(tuneUrl, mMPDData->mMPDDownloadResponse);
 			}
 		}
-		
 		if( IS_HTTP_SUCCESS(mMPDData->mMPDDownloadResponse->iHttpRetValue) )
 		{
 			if(!mMPDData->mMPDDownloadResponse->getString().empty())
