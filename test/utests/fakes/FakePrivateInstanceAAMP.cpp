00b22c50268281081cd22b5725cca918f65b0cc6
varshnie <varshniblue14@gmail.com>
2026-03-31 20:56:54 +0530

    VPLAY-12682:SoC-independent underflow detection - follow-up improvements
    Reason for change: SendBufferChangeEvent bufferingStop bool is changed to bufferingStart
    Test Procedure: No regression with buffering reporting
    Risks: Medium
    
    Signed-off-by: varshnie <varshniblue14@gmail.com>

--- test/utests/fakes/FakePrivateInstanceAAMP.cpp
+++ test/utests/fakes/FakePrivateInstanceAAMP.cpp
@@ -1606,7 +1606,7 @@ bool PrivateInstanceAAMP::PausePipeline(bool pause, bool forceStopGstreamerPreBu
 	return false;
 }
 
-void PrivateInstanceAAMP::SendBufferChangeEvent(bool bufferingStopped)
+void PrivateInstanceAAMP::SendBufferChangeEvent(bool bufferingStarted)
 {
 }
 
