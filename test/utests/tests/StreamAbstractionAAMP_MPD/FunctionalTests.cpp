3f3a5d11cb9b8379d542e4ad36b52f0dfca90397
Vinish100 <vinish.balan@gmail.com>
2026-04-01 17:30:02 +0530

    VPLAY-13199: LLD hardening - default configuration updates
    Reason for change: Reapply configuration updates -
    Update the default configurations for LLD as part of LLD and ABR hardening changes.
    Migrate latency to float to get millisecond precision.
    Test Procedure: Sanity testing LLD playback
    Risks: Medium
    
    Signed-off-by: Vinish100 <vinish.balan@gmail.com>

--- test/utests/tests/StreamAbstractionAAMP_MPD/FunctionalTests.cpp
+++ test/utests/tests/StreamAbstractionAAMP_MPD/FunctionalTests.cpp
@@ -2820,16 +2820,16 @@ TEST_F(FunctionalTests, ChunkMode_LLD_ForMaxLatency_Case)
 	EXPECT_CALL(*g_mockAampMPDDownloader, IsMPDLowLatency (_))
 		.WillRepeatedly(Return(true));
 
-	EXPECT_CALL(*g_mockAampConfig, GetConfigValue(testing::Matcher<AAMPConfigSettingInt>(_)))
-	.WillRepeatedly([](AAMPConfigSettingInt config) {
-	// Check if the config is maxLatencyConfig, return 9(default value); otherwise, return 0
+	EXPECT_CALL(*g_mockAampConfig, GetConfigValue(testing::Matcher<AAMPConfigSettingFloat>(_)))
+	.WillRepeatedly([](AAMPConfigSettingFloat config) {
+	// Check if the config is maxLatencyConfig, return 9 for the test usecase; otherwise, return 0
 	if (config == eAAMPConfig_LLMaxLatency) {
-		return 9;
+		return 9.0;
 	}
-	return 0;
+	return 0.0;
 	});
 
-	EXPECT_CALL(*g_mockAampConfig, GetConfigValue(testing::Matcher<AAMPConfigSettingFloat>(_))).WillRepeatedly(Return(0.0));
+	EXPECT_CALL(*g_mockAampConfig, GetConfigValue(testing::Matcher<AAMPConfigSettingInt>(_))).WillRepeatedly(Return(0));
 	//For this test case we need EnableLowLatencyDash as true
 	EXPECT_CALL(*g_mockAampConfig, IsConfigSet(_))
 		.WillRepeatedly(Return(false));
