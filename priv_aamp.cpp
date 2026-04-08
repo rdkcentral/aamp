69f3df30df89d5a677222bfa3cb48ff0b65c9c76
vkadun208_comcast <vinod_kadungoth2@comcast.com>
2026-04-08 16:24:13 -0400

    debug

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -6188,7 +6188,7 @@ void PrivateInstanceAAMP::Tune(const char *mainManifestUrl,
 		{
 			snprintf(tuneStrPrefix, sizeof(tuneStrPrefix), "%s PLAYER[%d]", (mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId);
 		}
-		AAMPLOG_MIL("%s aamp_tune: attempt: %d format: %s URL: %s", tuneStrPrefix, mTuneAttempts, mMediaFormatName[mMediaFormat], mManifestUrl.c_str());
+		AAMPLOG_MIL("%s aamp_tune:  vk:: attempt: %d format: %s URL: %s", tuneStrPrefix, mTuneAttempts, mMediaFormatName[mMediaFormat], mManifestUrl.c_str());
 		if(!mMPDStichRefreshUrl.empty())
 		{
 			AAMPLOG_WARN("%s aamp_stich: Option[%d] URL: %s", tuneStrPrefix, mMPDStichOption, mMPDStichRefreshUrl.c_str());
