7f0a4b18bf64ba103d41a0bda8c99f749ad159ce
lashminthajayanathan <lashmintha_jayanathan@comcast.com>
2026-04-10 16:54:46 +0530

    VPLAY-13297 Need to convert the log for "buffering duration" from INFO to WARN
    Reason for change: Converting buffering duration print from info to warn
    Risks: Low
    Test Procedure: Refer jira ticket
    Priority: P1
    
    Signed-off-by: lashminthaj <lashminthaj17@gmail.com>

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -3274,7 +3274,7 @@ void PrivateInstanceAAMP::SendBufferChangeEvent(bool bufferingStarted)
 		}
 	}
 
-	AAMPLOG_INFO("PrivateInstanceAAMP: Sending Buffer Change event status (Buffering): %s durationMs: %lld", (e->buffering() ? "End": "Start"), bufferingDurationMs);
+	AAMPLOG_WARN("PrivateInstanceAAMP: Sending Buffer Change event status (Buffering): %s durationMs: %lld", (e->buffering() ? "End": "Start"), bufferingDurationMs);
 #ifdef AAMP_TELEMETRY_SUPPORT
 	AAMPTelemetry2 at2(mAppName);
 	std::string telemetryName = bufferingStarted?"VideoBufferingStart":"VideoBufferingEnd";
