7b92b2802052212195f4e5a9f944e6bec8552350
Gnanesha <gnaani82@gmail.com>
2026-04-24 11:36:38 -0400

    RDKEMW-19029: Observed Video Freeze & Fast-Forward Effect After Channel Change
    Reason for change: Added retry logic to AAMP downloads when curl returns error 55 (CURL_SEND_ERROR)
    Risks: Low
    Test Procedure: Refer jira ticket
    Priority: P0
    
    Signed-off-by: Gnanesha <gnaani82@gmail.com>

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -5129,7 +5129,7 @@ bool PrivateInstanceAAMP::GetFile( std::string remoteUrl, AampMediaType mediaTyp
 						print_headerResponse(context.allResponseHeaders, mediaType);
 
 					}
-					if (res == CURLE_COULDNT_CONNECT || IsCurlTimeoutFailure(res) || (isDownloadStalled && (eCURL_ABORT_REASON_LOW_BANDWIDTH_TIMEDOUT != abortReason)))
+					if (res == CURLE_COULDNT_CONNECT || IsCurlTimeoutFailure(res) || (isDownloadStalled && (eCURL_ABORT_REASON_LOW_BANDWIDTH_TIMEDOUT != abortReason))|| res == CURLE_SEND_ERROR )
 					{
 
 						if(mpStreamAbstractionAAMP)
@@ -5193,7 +5193,7 @@ bool PrivateInstanceAAMP::GetFile( std::string remoteUrl, AampMediaType mediaTyp
 								break;
 							}
 						}
-						AAMPLOG_WARN("Download failed due to curl timeout or isDownloadStalled:%d Retrying:%d Attempt:%d abortReason:%d", isDownloadStalled, loopAgain && (downloadAttempt < maxDownloadAttempt), downloadAttempt, abortReason);
+						AAMPLOG_WARN("Download failed due to curl error %d or isDownloadStalled:%d Retrying:%d Attempt:%d abortReason:%d", res, isDownloadStalled, loopAgain && (downloadAttempt < maxDownloadAttempt), downloadAttempt, abortReason);
 					}
 
 					/*
