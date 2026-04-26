59aa222ef54bf1d966f1ea69d1ff84b2643c9672
Philip Stroffolino <philip_stroffolino@cable.comcast.com>
2026-04-26 18:46:58 -0400

    Reason for Change: Include AampSpeedCache.h if available (AAMP build); otherwise define SpeedCache inline so that consumers like FOG, which install only HybridABRManager.h remain buildable without needing AampSpeedCache.h as a separate installed header.
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- support/aampabr/HybridABRManager.h
+++ support/aampabr/HybridABRManager.h
@@ -27,7 +27,39 @@
 #include <string>
 #include <cstdio>
 #include "ABRManager.h"
-#include "AampSpeedCache.h"
+
+// Include AampSpeedCache.h if available (AAMP build); otherwise define SpeedCache inline
+// so that consumers like FOG, which install only HybridABRManager.h remain buildable without needing AampSpeedCache.h as a separate installed header.
+#ifdef __has_include
+#  if __has_include("AampSpeedCache.h")
+#    include "AampSpeedCache.h"
+#  endif
+#else
+#  include "AampSpeedCache.h"
+#endif
+
+#ifndef AAMP_SPEED_CACHE_H
+#define AAMP_SPEED_CACHE_H
+#include <utility>
+struct SpeedCache
+{
+	long last_sample_time_val;
+	long prev_dlnow;
+	long prevSampleTotalDownloaded;
+	long totalDownloaded;
+	long speed_now;
+	long start_val;
+	bool bStart;
+
+	double totalWeight;
+	double weightedBitsPerSecond;
+	std::vector< std::pair<double,long> > mChunkSpeedData;
+
+	SpeedCache() : last_sample_time_val(0), prev_dlnow(0), prevSampleTotalDownloaded(0), totalDownloaded(0), speed_now(0), start_val(0), bStart(false), totalWeight(0), weightedBitsPerSecond(0), mChunkSpeedData()
+	{
+	}
+};
+#endif /* AAMP_SPEED_CACHE_H */
 
 class HybridABRManager:public ABRManager
 {
