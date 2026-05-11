4b75eef27717d9e7d528d44f3448badc6e98bfbb
sayanurag <sayanurag665new@gmail.com>
2026-05-11 17:07:06 +0100

    VPAAMP-239 Change the CURLOPT_DNS_CACHE_TIMEOUT config (#1393)
    Reason for Change: Change the CURLOPT_DNS_CACHE_TIMEOUT config to be updated from config not as default
    * Add L1 test verifying CURLOPT_DNS_CACHE_TIMEOUT uses config value
    * AampCurlDefine.h: clarify DEFAULT_DNS_CACHE_TIMEOUT comment (180s = 3x libcurl default of 60s)
    * ( FakeCurl.cpp: route CURLOPT_DNS_CACHE_TIMEOUT through curl_easy_setopt_long mock
    * ( FunctionalTests.cpp: add DnsCacheTimeout_PassedFromDownloadConfig_NotHardCoded
      regression test; catches any future hard-coding of dns_cache_timeout instead of
      reading iDnsCacheTimeOut from DownloadConfig
    * FunctionalTests.cpp SetUp: allow curl_easy_setopt_long by default so existing
      tests are not broken by the new FakeCurl routing
    
    Test Procedure: refer ticket
    Risks: Low
    Priority: P1
    
    ---------
    
    Signed-off-by: Anurag Krishnan <akrish513@cable.comcast.com>
    Co-authored-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- downloader/AampCurlDefine.h
+++ downloader/AampCurlDefine.h
@@ -37,12 +37,7 @@
 
 #define DEFAULT_CURL_TIMEOUT 5L		/**< Default timeout for Curl downloads */
 #define DEFAULT_CURL_CONNECTTIMEOUT 3L	/**< Curl socket connection timeout */
-#define DEFAULT_DNS_CACHE_TIMEOUT 3*60L	/***< Name resolve results for this number of seconds*/
-/**
- * @enum AampCurlStoreErrorCode
- * @brief Error codes returned by curlstore
- */
- 
+#define DEFAULT_DNS_CACHE_TIMEOUT 3*60L	/**< Name resolve results cached for this many seconds (180 s = 3x the libcurl default of 60 s) */
 
 /**
  * @brief Http Header Type
