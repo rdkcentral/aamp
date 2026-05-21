6fdac55d9909439a3ba5b06847c71850202adaa6
Jose Fagundez <jfagunde@synamedia.com>
2026-05-21 09:46:47 +0200

    Add automake and libtool dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -163,6 +163,8 @@ function install_pkgs_linux_fn()
     install_package_fn libtinyxml2-dev
     install_package_fn openjdk-21-jre-headless
     install_package_fn libglib2.0-dev
+    install_package_fn automake
+    install_package_fn libtool
 
     VER=$(grep -oP 'VERSION_ID="\K[\d.]+' /etc/os-release)
 
