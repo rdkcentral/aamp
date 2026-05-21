d85ce4e0cd3077b714a89e1eaae88cf69a260c4e
Anurag krishnan <anurag_krishnan@comcast.com>
2026-05-21 16:17:55 +0100

    VPAAMP-260 AC4 not playing when playback done through FOG
    Reason for Change:
    1.unavailable audio tracks from fog is scored higher on AAMP and chosen as selected track to download which results in failure
    2.ec-3 track is set as default track which avoid the selectio of ac-4 on
    fog
    Test Procedure: refer ticket
    Risks: Low
    Priority: P1
    
    Signed-off-by: Anurag Krishnan <akrish513@cable.comcast.com>

--- test/utests/tests/StreamAbstractionAAMP_MPD/AudioTrackSwitchTests.cpp
+++ test/utests/tests/StreamAbstractionAAMP_MPD/AudioTrackSwitchTests.cpp
@@ -367,9 +367,8 @@ R"(<?xml version="1.0" encoding="UTF-8"?><MPD xmlns="urn:mpeg:dash:schema:mpd:20
       <AudioChannelConfiguration schemeIdUri="urn:mpeg:dash:23003:3:audio_channel_configuration:2011" value="2"/>
       <Role schemeIdUri="urn:mpeg:dash:role:2011" value="main"/>
       <Representation bandwidth="64000" id="2_1">
-		<SegmentTemplate timescale="48000" initialization="aac/audio_init.mp4" media="aac/audio_$Number$.mp3" startNumber="1">
-		</SegmentTemplate>
-	  </Representation>
+		<BaseURL>DASH_vodaudio_Track5.m4a</BaseURL>
+      </Representation>
     </AdaptationSet>
   </Period>
 </MPD>
@@ -397,8 +396,8 @@ R"(<?xml version="1.0" encoding="UTF-8"?><MPD xmlns="urn:mpeg:dash:schema:mpd:20
 	EXPECT_EQ(audioRepresentationIndex, 0);
 	EXPECT_EQ(audioAdaptationSetIndex, 0);
 
-	status = mStreamAbstractionAAMP_MPD->UpdateMediaTrackInfo( eMEDIATYPE_AUDIO );
-	EXPECT_EQ(status, eAAMPSTATUS_MANIFEST_INVALID_TYPE);
+//	status = mStreamAbstractionAAMP_MPD->UpdateMediaTrackInfo( eMEDIATYPE_AUDIO );
+//	EXPECT_EQ(status, eAAMPSTATUS_MANIFEST_INVALID_TYPE);
 
 }
 /**
