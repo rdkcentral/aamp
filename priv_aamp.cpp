abe746c1bfd1ad4153e7ddddfd26342f355d10d6
anjali-syna <206662904+anjali-syna@users.noreply.github.com>
2026-06-09 11:17:04 +0530

    Adding eAAMPConfig_useRialtoDirect config

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -1840,7 +1840,7 @@ PrivateInstanceAAMP::PrivateInstanceAAMP(AampConfig *config) : mReportProgressPo
 
 	// Ensure the correct CC variant class will be used
 	PlayerCCManager::SetRialto(GETCONFIGVALUE_PRIV(eAAMPConfig_useRialtoSink),
-					GETCONFIGVALUE_PRIV(eAAMPConfig_useDirectRialto));
+					GETCONFIGVALUE_PRIV(eAAMPConfig_useRialtoDirect));
 
 	preferredLanguagesString = GETCONFIGVALUE_PRIV(eAAMPConfig_PreferredAudioLanguage);
 	preferredRenditionString = GETCONFIGVALUE_PRIV(eAAMPConfig_PreferredAudioRendition);
