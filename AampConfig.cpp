a32435a5930f893fec62747140bd76b5bfe2caac
Philip Stroffolino <philip_stroffolino@cable.comcast.com>
2026-06-10 09:51:32 -0400

    VPAAMP-550 default enable mp4demux in sprint (over qtdemux)
    Reason for Change: we believe the last few functional gaps with mp4demux have been fixed.  enabling by default in June sprint, to ensure soak time in case any lingering subtle regressions
    
    Risk: Medium
    
    Test Guidance: regression-only
    
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -371,7 +371,7 @@ static const ConfigLookupEntryBool mConfigLookupTableBool[AAMPCONFIG_BOOL_COUNT]
 	{false, "monitorAV", eAAMPConfig_MonitorAV, true},
 	{false, "enablePTSRestampForHlsTs", eAAMPConfig_HlsTsEnablePTSReStamp, true},
 	{true, "overrideMediaHeaderDuration", eAAMPConfig_OverrideMediaHeaderDuration, true},
-	{false, "useMp4Demux", eAAMPConfig_UseMp4Demux,false },
+	{true, "useMp4Demux", eAAMPConfig_UseMp4Demux,false },
 	{false, "curlThroughput", eAAMPConfig_CurlThroughput, false },
 	{false, "useFireboltSDK", eAAMPConfig_UseFireboltSDK, false},
 	{true, "enableChunkInjection", eAAMPConfig_EnableChunkInjection, true},
