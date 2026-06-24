44656479e57451c2f3475a104803be30d4f2159a
Gnanesha <gnaani82@gmail.com>
2026-06-24 00:33:14 -0400

    VPAAMP-565: Deprecate ceaFormat from AAMP Config.
    Reason for Change: Deprecated ceaFormat from AAMP
    
    Test Procedure: see ticket
    
    Priority: P1
    
    Risks: Low

--- jsbindings/jsbindings.cpp
+++ jsbindings/jsbindings.cpp
@@ -498,7 +498,7 @@ static bool AAMP_setProperty_preferredCEAFormat(JSContextRef context, JSObjectRe
 		return false;
 	}
 	preferredCEAFormat = (int)JSValueToNumber(context, value, exception);
-        LOG_WARN(pAAMP,"_aamp->SetCEAFormat context=%p  value=%d exception=%p(deprecated )",context, preferredCEAFormat, exception);
+		LOG_WARN(pAAMP, "preferredCEAFormat is deprecated; ignoring (context=%p value=%d exception=%p)", context, preferredCEAFormat, exception);
 	return true;
 }
 
