e0347daa04673fe33fdf473db0977ea21278d35a
ALSAMEEMA <alsameema4@gmail.com>
2026-06-24 22:51:10 +0530

    RDKEMW-20406:[Flex 2.0][Xione Charter][E040.012.00.8.4p13s1][SYST_ERR_TLP999]Delay in moving the PlayerPipeline to 'Playing' state even after receiving the first video frame.

--- middleware/drm/DrmSessionManager.cpp
+++ middleware/drm/DrmSessionManager.cpp
@@ -26,6 +26,8 @@
 #include "DrmSessionManager.h"
 #include "_base64.h"
 #include <iostream>
+#include <chrono>
+#include <atomic>
 #include "DrmHelper.h"
 #include <inttypes.h>
 #include "PlayerUtils.h"
@@ -431,7 +433,7 @@ DrmSession * DrmSessionManager::createDrmSession( int& responseCode,
 		}
 	}
 
-	long long drmElapsedMS = NOW_STEADY_TS_MS - drmStartMS;
+	long long drmElapsedMS = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() - drmStartMS;
 	--activeDrmCreations;
 	MW_LOG_MIL("[XSTLP-999-DBG][MAIN-LOOP-STARVATION] createDrmSession exit: took %lld ms, concurrent=%d, session=%p", drmElapsedMS, activeDrmCreations.load(), drmSession);
 	return drmSession;
