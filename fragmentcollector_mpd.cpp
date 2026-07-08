e61741e60dced7234416fd93c4f5c852302ff401
srikanthreddybijjam-comcast <srikanthreddybijjam.2000@gmail.com>
2026-07-08 18:50:19 +0530

    VPAAMP-542:Fix Hot CDVR to COld CDVR transitions. (#1699)
    Reason for change: Fixed aamp->IsLiveSTream() with mIsLiveManifest.
    Test Procedure: Refer jira ticket VPAAMP-394
    Priority: P1
    
    Signed-off-by: srikanthreddybijjam-comcast <srikanthreddybijjam.2000@gmail.com>
    Co-authored-by: pstroffolino <Philip_Stroffolino@cable.comcast.com>

--- fragmentcollector_mpd.cpp
+++ fragmentcollector_mpd.cpp
@@ -10456,7 +10456,7 @@ void StreamAbstractionAAMP_MPD::FetcherLoop()
 					// For live stream in ad break, avoid fetching next segment if current fragment time is exceeding the live edge
 					// This is to avoid unnecessary fetch and also to avoid fetching segments which are not expected to be played
 					const bool exceedsLiveEdge = mCdaiObject &&
-							aamp->IsLiveStream() &&
+							mIsLiveManifest &&
 							(AdState::IN_ADBREAK_AD_PLAYING == mCdaiObject->mAdState) &&
 							(mMediaStreamContext[trackIdx]->fragmentTime >= aamp->mAbsoluteEndPosition);
 
