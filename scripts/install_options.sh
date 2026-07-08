ac5330d577cd3ee2a7ef512a709e54b57f976b2e
psiva01 <sivasubramanian.patchaiperumal@ltts.com>
2026-07-08 15:59:05 +0530

    VPAAMP-725: use external middleware-player-interface repo for simulator builds
    Reason for change: Default middleware commit should be tip of develop branch
    
    Signed-off-by: psiva01 <sivasubramanian.patchaiperumal@ltts.com>

--- scripts/install_options.sh
+++ scripts/install_options.sh
@@ -19,7 +19,7 @@
 
 # default values
 OPTION_AAMP_BRANCH="dev_sprint_25_2"
-OPTION_MIDDLEWARE_PLAYER_INTERFACE_COMMIT_ID="4c1d90a24338c342599ac098e247a8d644ea28e1"
+OPTION_MIDDLEWARE_PLAYER_INTERFACE_COMMIT_ID="$(git ls-remote https://github.com/rdkcentral/middleware-player-interface.git refs/heads/develop | awk '{print $1}')"
 OPTION_PLAYER_INTERFACE_SOURCE="external"
 OPTION_MIDDLEWARE_PLAYER_INTERFACE_LOCAL_PATH=""
 OPTION_BUILD_DIR=""
