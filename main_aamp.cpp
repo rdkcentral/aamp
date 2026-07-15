2e6f00d51b0b845cb87609b0d60aeef650801a25
Gavin Parry <gavin_parry@comcast.com>
2026-07-15 13:16:19 +0000

    VPLAY-13653 - Defer CC Enable / Disable to avoid Stop delays
    Reason for change: If Streamlock is not available, then apply this setting later.
    This is to reduce delays during channel zapping, reducing likelihood of EPG timeouts.
    Note: This still blocks on Streamlock if in a playing related state (e.g. user invoked subtitle change), since deferred settings are applied during tune.
    
    This also includes some useful future debug for stop timings, since fixing this has pushed delays further into stop calls during channel zapping.
    E.g. Delays now seen acquiring the async task execution mutex during stop when an async task tune is still in progress, i.e. in SuspendScheduler.
    
    Test Procedure: Check that the EPG stop sequence is not timing out. We still may see the occasional AS timeout in Stop (which includes _setClosedCaptionStatus & _stop calls).

--- main_aamp.cpp
+++ main_aamp.cpp
@@ -335,6 +335,7 @@ void PlayerInstanceAAMP::Stop(bool sendStateChangeEvent, bool forceCleanup)
 {
 	if (aamp)
 	{
+		auto playerStopStartTime=NOW_STEADY_TS_MS;
 		UsingPlayerId playerId(aamp->mPlayerId);
 		AAMPPlayerState state = aamp->GetState();
 
@@ -342,7 +343,9 @@ void PlayerInstanceAAMP::Stop(bool sendStateChangeEvent, bool forceCleanup)
 		// 2. Check for state ,if already in Idle / Released , ignore stopInternal
 		// 3. Restart the scheduler , needed if same instance is used for tune again
 
+		auto suspendSchedulerStartTime=NOW_STEADY_TS_MS;
 		mScheduler.SuspendScheduler();
+		auto suspendSchedulerEndTime=NOW_STEADY_TS_MS;
 		mScheduler.RemoveAllTasks();
 
 		//state will be eSTATE_IDLE or eSTATE_RELEASED, right after an init or post-processing of a Stop call
@@ -361,6 +364,11 @@ void PlayerInstanceAAMP::Stop(bool sendStateChangeEvent, bool forceCleanup)
 		}
 		//Release lock
 		mScheduler.ResumeScheduler();
+		auto resumeSchedulerEndTime=NOW_STEADY_TS_MS;
+		AAMPLOG_WARN("-Stop (player) ; SuspendScheduler took %u ms, Total %u ms",
+				(unsigned)(suspendSchedulerEndTime-suspendSchedulerStartTime),
+				(unsigned)(resumeSchedulerEndTime-playerStopStartTime)
+			);
 	}
 }
 
