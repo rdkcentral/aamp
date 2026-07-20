e3265592a5e6d7a577f9fe2eb19b51d251aca40e
Abhi-jith-S <abhijithssa7@gmail.com>
2026-07-20 15:54:24 +0530

    Revert "VPAAMP-509 Fix missing override specifiers (#1547)"
    This reverts commit 203098b106caf8c7bae72c9dea1f56991dc22ffb.

--- test/utests/mocks/MockAampGstPlayer.h
+++ test/utests/mocks/MockAampGstPlayer.h
@@ -38,11 +38,11 @@ public:
 
     MOCK_METHOD(bool , SetTextStyle, (const std::string &options), (override));
 
-    MOCK_METHOD(void, ChangeAamp, (PrivateInstanceAAMP *, id3_callback_t), (override));
+    MOCK_METHOD(void, ChangeAamp, (PrivateInstanceAAMP *, id3_callback_t));
 
     MOCK_METHOD(void, Flush, (double position, int rate, bool shouldTearDown), (override));
 
-    MOCK_METHOD(void, SetEncryptedAamp, (PrivateInstanceAAMP *), (override));
+    MOCK_METHOD(void, SetEncryptedAamp, (PrivateInstanceAAMP *));
 
 	MOCK_METHOD(void, Stop, (bool), (override));
 
