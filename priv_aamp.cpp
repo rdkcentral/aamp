rdkcentral/aamp

commit bda30014bed262e5c4f72279908c98a2f1c42270

VPLAY-11225 [Tech Debt] mediafailed event enhancements ( minor code etc ) (#685)
VPLAY-11225 [Tech Debt] mediafailed event enhancements ( minor code etc )

Reason for change: Added minor code for MediaFailedEvent
Risks: Low
Test Procedure: Refer jira ticket
Priority: P1

Signed-off-by: lashmintha <lashminthaj17@gmail.com>

priv_aamp.cpp
@@ -170,53 +170,78 @@ struct CurlCbContextSyncTime
 struct TuneFailureMap
 {
     AAMPTuneFailure tuneFailure;    /**< Failure ID */
-    int code;                       /**< Error code */
+    int code;                       /**< Major Error code */
+	int subCode;					/**< Minor Error code */
     const char* description;        /**< Textual description */
 };
 
 static TuneFailureMap tuneFailureMap[] =
 {
-	{AAMP_TUNE_INIT_FAILED, 10, "AAMP: init failed"}, //"Fragmentcollector initialization failed"
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_DNLD_ERROR, 10, "AAMP: init failed (unable to download manifest)"},
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_CONTENT_ERROR, 10, "AAMP: init failed (manifest missing tracks)"},
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_PARSE_ERROR, 10, "AAMP: init failed (corrupt/invalid manifest)"},
-	{AAMP_TUNE_INIT_FAILED_PLAYLIST_VIDEO_DNLD_ERROR, 10, "AAMP: init failed (unable to download video playlist)"},
-	{AAMP_TUNE_INIT_FAILED_PLAYLIST_AUDIO_DNLD_ERROR, 10, "AAMP: init failed (unable to download audio playlist)"},
-	{AAMP_TUNE_INIT_FAILED_TRACK_SYNC_ERROR, 10, "AAMP: init failed (unsynchronized tracks)"},
-	{AAMP_TUNE_MANIFEST_REQ_FAILED, 10, "AAMP: Manifest Download failed"}, //"Playlist refresh failed"
-	{AAMP_TUNE_AUTHORIZATION_FAILURE, 40, "AAMP: Authorization failure"},
-	{AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE, 10, "AAMP: fragment download failures"},
-	{AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE, 10, "AAMP: init fragment download failed"},
-	{AAMP_TUNE_UNTRACKED_DRM_ERROR, 50, "AAMP: DRM error untracked error"},
-	{AAMP_TUNE_DRM_INIT_FAILED, 50, "AAMP: DRM Initialization Failed"},
-	{AAMP_TUNE_DRM_DATA_BIND_FAILED, 50, "AAMP: InitData-DRM Binding Failed"},
-	{AAMP_TUNE_DRM_SESSIONID_EMPTY, 50, "AAMP: DRM Session ID Empty"},
-	{AAMP_TUNE_DRM_CHALLENGE_FAILED, 50, "AAMP: DRM License Challenge Generation Failed"},
-	{AAMP_TUNE_LICENCE_TIMEOUT, 50, "AAMP: DRM License Request Timed out"},
-	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, "AAMP: DRM License Request Failed"},
-	{AAMP_TUNE_INVALID_DRM_KEY, 50, "AAMP: Invalid Key Error, from DRM"},
-	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"
-	{AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE, 60, "AAMP: No supported Audio Types in Manifest"},
-	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, "AAMP: Failed to parse key id from PSSH"},
-	{AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN, 50, "AAMP: Failed to get access token from Auth Service"},
-	{AAMP_TUNE_CORRUPT_DRM_DATA, 51, "AAMP: DRM failure due to Corrupt DRM files"},
-	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
-	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, "AAMP: DRM Decryption Failed for Fragments"},
-	{AAMP_TUNE_DRM_UNSUPPORTED, 50, "AAMP: DRM format Unsupported"},
-	{AAMP_TUNE_DRM_SELF_ABORT, 50, "AAMP: DRM license request aborted by player"},
-	{AAMP_TUNE_GST_PIPELINE_ERROR, 80, "AAMP: Error from gstreamer pipeline"},
-	{AAMP_TUNE_PLAYBACK_STALLED, 7600, "AAMP: Playback was stalled due to lack of new fragments"},
-	{AAMP_TUNE_CONTENT_NOT_FOUND, 20, "AAMP: Resource was not found at the URL(HTTP 404)"},
-	{AAMP_TUNE_DRM_KEY_UPDATE_FAILED, 50, "AAMP: Failed to process DRM key"},
-	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 52, "AAMP: Device not provisioned"},
-	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 53, "AAMP: HDCP Compliance Check Failure"},
-	{AAMP_TUNE_INVALID_MANIFEST_FAILURE, 10, "AAMP: Invalid Manifest, parse failed"},
-	{AAMP_TUNE_FAILED_PTS_ERROR, 80, "AAMP: Playback failed due to PTS error"},
-	{AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING, 10, "AAMP: init fragments missing in playlist"},
-	{AAMP_TUNE_DNS_RESOLVE_TIMEOUT, 10, "AAMP: Manifest download failed due to DNS resolve timeout"},
-	{AAMP_TUNE_CURL_CONNECTION_TIMEOUT, 10, "AAMP: Manifest download failed due to connection timeout"},
-	{AAMP_TUNE_DATA_TRANSFER_TIMEOUT, 10, "AAMP: Manifest download failed due to data transfer timeout"},
-	{AAMP_TUNE_FAILURE_UNKNOWN, 100, "AAMP: Unknown Failure"}
+	//Init failure
+	{AAMP_TUNE_INIT_FAILED, 10, 1, "AAMP: init failed"}, //"Fragmentcollector initialization failed"
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_DNLD_ERROR, 10, 2, "AAMP: init failed (unable to download manifest)"},
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_CONTENT_ERROR, 10, 3, "AAMP: init failed (manifest missing tracks)"},
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_PARSE_ERROR, 10, 4, "AAMP: init failed (corrupt/invalid manifest)"},
+	{AAMP_TUNE_INIT_FAILED_PLAYLIST_VIDEO_DNLD_ERROR, 10, 5, "AAMP: init failed (unable to download video playlist)"},
+	{AAMP_TUNE_INIT_FAILED_PLAYLIST_AUDIO_DNLD_ERROR, 10, 6, "AAMP: init failed (unable to download audio playlist)"},
+	{AAMP_TUNE_INIT_FAILED_TRACK_SYNC_ERROR, 10, 7, "AAMP: init failed (unsynchronized tracks)"},
+	
+	
+    //Resource failure
+	{AAMP_TUNE_CONTENT_NOT_FOUND, 20, 1, "AAMP: Resource was not found at the URL(HTTP 404)"},
+	
+	//Download failure
+	{AAMP_TUNE_MANIFEST_REQ_FAILED, 30, 1, "AAMP: Manifest Download failed"}, //"Playlist refresh failed"
+	{AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE, 30, 2, "AAMP: fragment download failures"},
+	{AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE, 30, 3, "AAMP: init fragment download failed"},
+	{AAMP_TUNE_INVALID_MANIFEST_FAILURE, 30, 4, "AAMP: Invalid Manifest, parse failed"},
+	{AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING, 30, 5, "AAMP: init fragments missing in playlist"},
+	{AAMP_TUNE_DNS_RESOLVE_TIMEOUT, 30, 6, "AAMP: Manifest download failed due to DNS resolve timeout"},
+	{AAMP_TUNE_CURL_CONNECTION_TIMEOUT, 30, 7, "AAMP: Manifest download failed due to connection timeout"},
+	{AAMP_TUNE_DATA_TRANSFER_TIMEOUT, 30, 8, "AAMP: Manifest download failed due to data transfer timeout"},
+
+
+	//Authorization failure
+	{AAMP_TUNE_AUTHORIZATION_FAILURE, 40, 1, "AAMP: Authorization failure"},
+
+	//DRM Failure
+	{AAMP_TUNE_UNTRACKED_DRM_ERROR, 50, 1, "AAMP: DRM error untracked error"},
+	{AAMP_TUNE_DRM_INIT_FAILED, 50, 2, "AAMP: DRM Initialization Failed"},
+	{AAMP_TUNE_DRM_DATA_BIND_FAILED, 50, 3, "AAMP: InitData-DRM Binding Failed"},
+	{AAMP_TUNE_DRM_SESSIONID_EMPTY, 50, 4, "AAMP: DRM Session ID Empty"},
+	{AAMP_TUNE_DRM_CHALLENGE_FAILED, 50, 5, "AAMP: DRM License Challenge Generation Failed"},
+	{AAMP_TUNE_LICENCE_TIMEOUT, 50, 6, "AAMP: DRM License Request Timed out"},
+	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, 7, "AAMP: DRM License Request Failed"},
+	{AAMP_TUNE_INVALID_DRM_KEY, 50, 8, "AAMP: Invalid Key Error, from DRM"},
+	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, 9, "AAMP: Failed to parse key id from PSSH"},
+	{AAMP_TUNE_CORRUPT_DRM_DATA, 50, 10, "AAMP: DRM failure due to Corrupt DRM files"},
+	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, 11, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
+	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, 12, "AAMP: DRM Decryption Failed for Fragments"},
+	{AAMP_TUNE_DRM_UNSUPPORTED, 50, 13, "AAMP: DRM format Unsupported"},
+	{AAMP_TUNE_DRM_SELF_ABORT, 50, 14, "AAMP: DRM license request aborted by player"},
+	{AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN, 50, 15, "AAMP: Failed to get access token from Auth Service"},
+	{AAMP_TUNE_DRM_KEY_UPDATE_FAILED, 50, 16, "AAMP: Failed to process DRM key"},
+
+	
+	//Provisioning failure
+	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 51, 1, "AAMP: Device not provisioned"},
+
+	//Hdcp failure
+	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 52, 1, "AAMP: HDCP Compliance Check Failure"},
+
+	//Stream failure
+	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, 1, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"
+	{AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE, 60, 2, "AAMP: No supported Audio Types in Manifest"},
+
+	//Gstreamer error
+	{AAMP_TUNE_GST_PIPELINE_ERROR, 80, 1, "AAMP: Error from gstreamer pipeline"},
+	{AAMP_TUNE_FAILED_PTS_ERROR, 80, 2, "AAMP: Playback failed due to PTS error"},
+
+	//Playback failure
+	{AAMP_TUNE_PLAYBACK_STALLED, 7600, 1, "AAMP: Playback was stalled due to lack of new fragments"},
+	
+	//Unknown failure
+	{AAMP_TUNE_FAILURE_UNKNOWN, 100, 1, "AAMP: Unknown Failure"}
 };
 
 static const std::pair<std::string , std::string> gCDAIErrorDetails[] = {
@@ -3011,7 +3036,7 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 	lock.unlock();
 	if (sendErrorEvent)
 	{
-		int code;
+		int code = AAMP_TUNE_FAILURE_UNKNOWN, subCode = 1;
 		const char *errorDescription = NULL;
 		DisableDownloads();
 		if(tuneFailure >= 0 && tuneFailure < AAMP_TUNE_FAILURE_UNKNOWN)
@@ -3024,6 +3049,7 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 			{
 				code = tuneFailureMap[tuneFailure].code;
 			}
+			subCode = tuneFailureMap[tuneFailure].subCode;
 			if(description)
 			{
 				errorDescription = description;
@@ -3036,17 +3062,18 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 		else
 		{
 			code = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].code;
+			subCode = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].subCode;
 			errorDescription = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].description;
 		}
-		MediaErrorEventPtr e = std::make_shared<MediaErrorEvent>(tuneFailure, code, errorDescription, isRetryEnabled, secManagerClassCode, secManagerReasonCode, secClientBusinessStatus, responseData, GetSessionId());
+		MediaErrorEventPtr e = std::make_shared<MediaErrorEvent>(tuneFailure, code, subCode, errorDescription, isRetryEnabled, secManagerClassCode, secManagerReasonCode, secClientBusinessStatus, responseData, GetSessionId());
 		SendAnomalyEvent(ANOMALY_ERROR, "Error[%d]:%s", tuneFailure, e->getDescription().c_str());
 		if (!mAppName.empty())
 		{
-			AAMPLOG_ERR("%s PLAYER[%d] APP: %s Sending error %s",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, mAppName.c_str(), e->getDescription().c_str());
+			AAMPLOG_ERR("%s PLAYER[%d] APP: %s Sending error %s code [%d:%d]",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, mAppName.c_str(), e->getDescription().c_str(),code, subCode);
 		}
 		else
 		{
-			AAMPLOG_ERR("%s PLAYER[%d] Sending error %s",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, e->getDescription().c_str());
+			AAMPLOG_ERR("%s PLAYER[%d] Sending error %s code [%d:%d]",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, e->getDescription().c_str(), code, subCode);
 		}
 
 		if (rate != AAMP_NORMAL_PLAY_RATE)

AampEvent.cpp
@@ -49,8 +49,8 @@ AAMPEventType AAMPEventObject::getType() const
 /**
  * @brief MediaErrorEvent Constructor
  */
-MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
-		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid)), mFailure(failure), mCode(code),
+MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
+		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid)), mFailure(failure), mCode(code), mSubCode(subCode),
 		mDescription(desc), mShouldRetry(shouldRetry), mSecManagerClass(classCode), mSecManagerReasonCode(reason), mBusinessStatus(businessStatus), mResponseData(responseData)
 {
 
@@ -67,15 +67,23 @@ AAMPTuneFailure MediaErrorEvent::getFailure() const
 }
 
 /**
- * @brief Get Error Code
- *
- * @return Tune error code
+ * @brief Get the error sub code associated with this media error event.
+ * 
+ * @return Error sub code value for this media error.
  */
 int MediaErrorEvent::getCode() const
 {
 	return mCode;
 }
-
+/**
+ * @brief Get the error code associated with this media error event.
+ * 
+ * @return Error code value for this media error
+ */
+int MediaErrorEvent::getSubCode() const
+{
+	return mSubCode;
+}
 /**
  * @brief Get Description
  *

AampEvent.h
@@ -123,40 +123,63 @@ typedef enum
 	AAMP_TUNE_INIT_FAILED_PLAYLIST_VIDEO_DNLD_ERROR,   /**< Tune failure due to video playlist download error*/
 	AAMP_TUNE_INIT_FAILED_PLAYLIST_AUDIO_DNLD_ERROR,   /**< Tune failure due to audio playlist download error*/
 	AAMP_TUNE_INIT_FAILED_TRACK_SYNC_ERROR,	           /**< Tune failure due to A/V track sync error*/
-	AAMP_TUNE_MANIFEST_REQ_FAILED,			   /**< Tune failure caused by manifest fetch failure*/
-	AAMP_TUNE_AUTHORIZATION_FAILURE,		   /**< Not authorized to view the content*/
-	AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE,		   /**< When fragment download fails for 5 consecutive fragments*/
-	AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE,	   /**< Unable to download init fragment*/
-	AAMP_TUNE_UNTRACKED_DRM_ERROR,			   /**< DRM error*/
-	AAMP_TUNE_DRM_INIT_FAILED,			   /**< DRM initialization failure */
-	AAMP_TUNE_DRM_DATA_BIND_FAILED,			   /**< InitData binding with DRM failed */
-	AAMP_TUNE_DRM_SESSIONID_EMPTY,			   /**< DRM session ID empty */
-	AAMP_TUNE_DRM_CHALLENGE_FAILED,			   /**< DRM key request challenge generation failed */
-	AAMP_TUNE_LICENCE_TIMEOUT,			   /**< DRM license request timeout */
-	AAMP_TUNE_LICENCE_REQUEST_FAILED,		   /**< DRM license got invalid response */
-	AAMP_TUNE_INVALID_DRM_KEY,			   /**< DRM reporting invalid license key */
-	AAMP_TUNE_UNSUPPORTED_STREAM_TYPE,		   /**< Unsupported stream type */
-	AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE,		   /**< Unsupported audio type in manifest */
-	AAMP_TUNE_FAILED_TO_GET_KEYID,			   /**< Failed to parse key id from init data*/
-	AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN,		   /**< Failed to get session token from AuthService*/
-	AAMP_TUNE_CORRUPT_DRM_DATA,			   /**< DRM failure due to corrupt drm data, self heal might clear further errors*/
-	AAMP_TUNE_CORRUPT_DRM_METADATA,			   /**< DRM failure due to corrupt drm metadata in the stream*/
-	AAMP_TUNE_DRM_DECRYPT_FAILED,			   /**< DRM Decryption Failed for Fragments */
-	AAMP_TUNE_DRM_UNSUPPORTED,			   /**< DRM Format Unsupported */
-	AAMP_TUNE_DRM_SELF_ABORT,			   /**< Download activity is aborted by player */
-	AAMP_TUNE_GST_PIPELINE_ERROR,			   /**< Playback failure due to error from GStreamer pipeline or associated plugins */
-	AAMP_TUNE_PLAYBACK_STALLED,			   /**< Playback was stalled due to valid fragments not available in playlist */
-	AAMP_TUNE_CONTENT_NOT_FOUND,			   /**< The resource was not found at the URL provided (HTTP 404) */
-	AAMP_TUNE_DRM_KEY_UPDATE_FAILED,		   /**< Failed to process DRM key, see the error code returned from Update() for more info */
-	AAMP_TUNE_DEVICE_NOT_PROVISIONED,		   /**< STB not provisioned/corrupted; need to re-provision. */
-	AAMP_TUNE_HDCP_COMPLIANCE_ERROR,		   /**< HDCP Compliance Check failure.Not compatible hdcp version for playback */
-	AAMP_TUNE_INVALID_MANIFEST_FAILURE,		   /**< Manifest is invalid */
-	AAMP_TUNE_FAILED_PTS_ERROR,			   /**< Playback failed due to PTS error */
-	AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING,		   /**< Init fragments missing in playlist */
-	AAMP_TUNE_DNS_RESOLVE_TIMEOUT,
-	AAMP_TUNE_CURL_CONNECTION_TIMEOUT,
-	AAMP_TUNE_DATA_TRANSFER_TIMEOUT,
-	AAMP_TUNE_FAILURE_UNKNOWN		   /**<  Unknown failure */
+
+	
+	//Resource failure
+	AAMP_TUNE_CONTENT_NOT_FOUND,	/**< The resource was not found at the URL provided (HTTP 404) */
+	
+	//Download failure
+	AAMP_TUNE_MANIFEST_REQ_FAILED,  /**< Tune failure caused by manifest fetch failure*/
+	AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE, /**< When fragment download fails for 5 consecutive fragments*/
+	AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE, 	/**< Unable to download init fragment*/
+	AAMP_TUNE_INVALID_MANIFEST_FAILURE,  /**< Manifest is invalid */
+	AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING, /**< Init fragments missing in playlist */
+	AAMP_TUNE_DNS_RESOLVE_TIMEOUT, /**< DNS resolve timedout */
+	AAMP_TUNE_CURL_CONNECTION_TIMEOUT, /**< Curl connection timedout */
+	AAMP_TUNE_DATA_TRANSFER_TIMEOUT, /**< Data transfer timedout */
+
+	//Authorization failure
+	AAMP_TUNE_AUTHORIZATION_FAILURE, 	 /**< Not authorized to view the content*/
+
+	//DRM Failure
+	AAMP_TUNE_UNTRACKED_DRM_ERROR, 	/**< DRM error*/
+	AAMP_TUNE_DRM_INIT_FAILED, 	/**< DRM initialization failure */
+	AAMP_TUNE_DRM_DATA_BIND_FAILED, 	/**< InitData binding with DRM failed */
+	AAMP_TUNE_DRM_SESSIONID_EMPTY,		/**< DRM session ID empty */
+	AAMP_TUNE_DRM_CHALLENGE_FAILED,		/**< DRM key request challenge generation failed */
+	AAMP_TUNE_LICENCE_TIMEOUT, 	/**< DRM license request timeout */
+	AAMP_TUNE_LICENCE_REQUEST_FAILED,  /**< DRM license got invalid response */
+	AAMP_TUNE_INVALID_DRM_KEY, /**< DRM reporting invalid license key */
+	AAMP_TUNE_FAILED_TO_GET_KEYID, /**< Failed to parse key id from init data*/
+	AAMP_TUNE_CORRUPT_DRM_DATA, /**< DRM failure due to corrupt drm data, self heal might clear further errors*/
+	AAMP_TUNE_CORRUPT_DRM_METADATA, /**< DRM failure due to corrupt drm metadata in the stream*/
+	AAMP_TUNE_DRM_DECRYPT_FAILED, /**< DRM Decryption Failed for Fragments */
+	AAMP_TUNE_DRM_UNSUPPORTED, 	/**< DRM Format Unsupported */
+	AAMP_TUNE_DRM_SELF_ABORT, /**< Download activity is aborted by player */
+	AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN,	/**< Failed to get session token from AuthService*/
+	AAMP_TUNE_DRM_KEY_UPDATE_FAILED,	/**< Failed to process DRM key, see the error code returned from Update() for more info */
+
+    
+	
+	//Provisioning failure
+	AAMP_TUNE_DEVICE_NOT_PROVISIONED,  /**< STB not provisioned/corrupted; need to re-provision. */
+
+	//Hdcp failure
+	AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 	/**< HDCP Compliance Check failure.Not compatible hdcp version for playback */
+
+	//Stream failure
+	AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 	/**< Unsupported stream type */
+	AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE,	/**< Unsupported audio type in manifest */
+
+	//Gstreamer error
+	AAMP_TUNE_GST_PIPELINE_ERROR, /**< Playback failed due to an error in the GStreamer pipeline */
+	AAMP_TUNE_FAILED_PTS_ERROR,  /**< Playback failed due to PTS error */
+
+	//Playback failure
+	AAMP_TUNE_PLAYBACK_STALLED, /**< Playback was stalled due to valid fragments not available in playlist */
+	
+	//Unknown failure
+	AAMP_TUNE_FAILURE_UNKNOWN
 } AAMPTuneFailure;
 
 /**
@@ -383,7 +406,8 @@ struct AAMPEvent
 		struct
 		{
 			AAMPTuneFailure failure;                            /**< Error Type */
-			int code;                                           /**< Error code */
+			int code;											/**< Error code */
+			int subCode;                                        /**< Error sub code */
 			const char *description;                            /**< Error description */
 			bool shouldRetry;                                   /**< If recovery on retry is possible */
 			int32_t classCode; /**< class Code */
@@ -620,6 +644,7 @@ class MediaErrorEvent: public AAMPEventObject
 {
 	AAMPTuneFailure mFailure;	/**< Error Type */
 	int mCode;			/**< Error code */
+	int mSubCode;		/**< Error sub code */
 	std::string mDescription;	/**< Error description */
 	std::string mResponseData;	/**< Response Data */
 	bool mShouldRetry;		/**< If recovery on retry is possible */
@@ -638,10 +663,16 @@ public:
 	 *
 	 * @param[in] failure     - Failure type
 	 * @param[in] code        - Error code
+	 * @param[in] subCode     - Error sub code
 	 * @param[in] desc        - Error description
 	 * @param[in] shouldRetry - Retry or not
+	 * @param[in] classCode      - Secmanager error class code
+	 * @param[in] reason         - Secmanager reason code
+	 * @param[in] businessStatus - Secclient business status code
+	 * @param[in] responseData   - Response data associated with the error
+	 * @param[in] sid            - Session identifier associated with the error
 	 */
-	MediaErrorEvent(AAMPTuneFailure failure, int code, const std::string &desc, bool shouldRetry, int32_t classCode, int32_t reason, int32_t businessStatus, const std::string &responseData, std::string sid);
+	MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int32_t classCode, int32_t reason, int32_t businessStatus, const std::string &responseData, std::string sid);
 
 	/**
 	 * @brief MediaErrorEvent Destructor
@@ -654,10 +685,17 @@ public:
 	AAMPTuneFailure getFailure() const;
 
 	/**
+	 * @fn getSubCode
+	 * @brief Get the error sub code associated with this media error event.
+	 * @return Error sub code value for this media error.
+	 */
+	int getSubCode() const;
+	/**
 	 * @fn getCode
+	 * @brief Get the error code associated with this media error event.
+	 * @return Error code value for this media error.
 	 */
 	int getCode() const;
-
 	/**
 	 * @fn getDescription
 	 */

AampEventListener.cpp
@@ -42,6 +42,7 @@ static void GenerateLegacyAAMPEvent(const AAMPEventPtr &e, AAMPEvent &event)
 			MediaErrorEventPtr ev = std::dynamic_pointer_cast<MediaErrorEvent>(e);
 			event.data.mediaError.failure = ev->getFailure();
 			event.data.mediaError.code = ev->getCode();
+			event.data.mediaError.subCode = ev->getSubCode();
 			event.data.mediaError.description = ev->getDescription().c_str();
 			event.data.mediaError.shouldRetry = ev->shouldRetry();
 			if(-1 != ev->getClass()) //Only send the verbose logging for DRM failure due to secclient/secmanager

jsbindings/jsbindings.cpp
@@ -928,12 +928,16 @@ public:
 	void setEventProperties(const AAMPEventPtr& e, JSContextRef context, JSObjectRef eventObj)
 	{
 		MediaErrorEventPtr evt = std::dynamic_pointer_cast<MediaErrorEvent>(e);
-
 		int code = evt->getCode();
+		int subCode = evt->getSubCode();
 		const char* description = evt->getDescription().c_str();
 
 		JSStringRef name = JSStringCreateWithUTF8CString("code");
 		JSObjectSetProperty(context, eventObj, name, JSValueMakeNumber(context, code), kJSPropertyAttributeReadOnly, NULL);
+		JSStringRelease(name);
+
+		name = JSStringCreateWithUTF8CString("subCode");
+		JSObjectSetProperty(context, eventObj, name, JSValueMakeNumber(context, subCode), kJSPropertyAttributeReadOnly, NULL);
 		JSStringRelease(name);
 
 		name = JSStringCreateWithUTF8CString("description");

jsbindings/jseventlistener.cpp
@@ -261,6 +261,10 @@ public:
 		JSObjectSetProperty(p_obj->_ctx, jsEventObj, prop, JSValueMakeNumber(p_obj->_ctx, evt->getCode()), kJSPropertyAttributeReadOnly, NULL);
 		JSStringRelease(prop);
 
+		prop = JSStringCreateWithUTF8CString("subCode");
+		JSObjectSetProperty(p_obj->_ctx, jsEventObj, prop, JSValueMakeNumber(p_obj->_ctx, evt->getSubCode()), kJSPropertyAttributeReadOnly, NULL);
+		JSStringRelease(prop);
+
 		prop = JSStringCreateWithUTF8CString("description");
 		JSObjectSetProperty(p_obj->_ctx, jsEventObj, prop, aamp_CStringToJSValue(p_obj->_ctx, evt->getDescription().c_str()), kJSPropertyAttributeReadOnly, NULL);
 		JSStringRelease(prop);

test/aampcli/Aampcli.cpp
@@ -505,7 +505,7 @@ void MyAAMPEventListener::Event(const AAMPEventPtr& e)
 			{
 				MediaErrorEventPtr ev = std::dynamic_pointer_cast<MediaErrorEvent>(e);
 				mAampcli.mTuneFailureDescription = ev->getDescription();
-				AAMPCLI_PRINTF("[AAMPCLI] AAMP_EVENT_TUNE_FAILED reason=%s\n",mAampcli.mTuneFailureDescription.c_str());
+				AAMPCLI_PRINTF("[AAMPCLI] AAMP_EVENT_TUNE_FAILED reason=%s code [%d:%d]\n",mAampcli.mTuneFailureDescription.c_str(),ev->getCode(), ev->getSubCode());
 				break;
 			}
 		case AAMP_EVENT_SPEED_CHANGED:

test/utests/fakes/FakeAampEvent.cpp
@@ -328,7 +328,7 @@ int SupportedSpeedsChangedEvent::getSupportedSpeedCount() const
 	return 0;
 }
 
-MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
+MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
 		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid))
 {
 }
@@ -336,6 +336,7 @@ bool MediaErrorEvent::shouldRetry(void) const { return false; }
 int32_t MediaErrorEvent::getBusinessStatus(void) const { return 0; }
 int32_t MediaErrorEvent::getClass(void) const { return 0; }
 int MediaErrorEvent::getCode(void) const { return 0; }
+int MediaErrorEvent::getSubCode(void) const { return 0; }
 
 BitrateChangeEvent::BitrateChangeEvent(int time, BitsPerSecond bitrate, const std::string &desc, int width, int height, double frameRate, double position, bool cappedProfile, int displayWidth, int displayHeight, VideoScanType videoScanType, int aspectRatioWidth, int aspectRatioHeight, std::string sid):
 		AAMPEventObject(AAMP_EVENT_BITRATE_CHANGED, std::move(sid))

commit 307d276b6405ce6328325077adab1aae60f33d1f

VPAAMP-918  : Fix error code regression: restore backward compatibility for codes 51–53
Reason for change:

Commit bda30014 (VPLAY-11225) consolidated CORRUPT_DRM_DATA into the DRM error
group (50/10), inadvertently cascading a −1 shift on codes 52 and 53. This broke
the published API contract for external clients matching on error codes.

Restore original major codes while retaining subCode field:
- CORRUPT_DRM_DATA:       50,10 → 51,1
- DEVICE_NOT_PROVISIONED: 51,1 → 52,1
- HDCP_COMPLIANCE_ERROR:  52,1 → 53,1

External consumers matching event.code === 51|52|53 now work correctly again.
Internal DRM error grouping semantics preserved via subCode field.

Fixes: VPLAY-11225 (regression from bda30014)

Test Procedure:

Priority: P2

Risks:Low

priv_aamp.cpp
@@ -222,7 +222,7 @@ static TuneFailureMap tuneFailureMap[] =
 	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, 7, "AAMP: DRM License Request Failed"},
 	{AAMP_TUNE_INVALID_DRM_KEY, 50, 8, "AAMP: Invalid Key Error, from DRM"},
 	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, 9, "AAMP: Failed to parse key id from PSSH"},
-	{AAMP_TUNE_CORRUPT_DRM_DATA, 50, 10, "AAMP: DRM failure due to Corrupt DRM files"},
+	{AAMP_TUNE_CORRUPT_DRM_DATA, 51, 1, "AAMP: DRM failure due to Corrupt DRM files"},
 	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, 11, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
 	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, 12, "AAMP: DRM Decryption Failed for Fragments"},
 	{AAMP_TUNE_DRM_UNSUPPORTED, 50, 13, "AAMP: DRM format Unsupported"},
@@ -233,10 +233,10 @@ static TuneFailureMap tuneFailureMap[] =
 
 
 	//Provisioning failure
-	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 51, 1, "AAMP: Device not provisioned"},
+	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 52, 1, "AAMP: Device not provisioned"},
 
 	//Hdcp failure
-	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 52, 1, "AAMP: HDCP Compliance Check Failure"},
+	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 53, 1, "AAMP: HDCP Compliance Check Failure"},
 
 	//Stream failure
 	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, 1, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"