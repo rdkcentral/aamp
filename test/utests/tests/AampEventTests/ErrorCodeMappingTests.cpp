rdkcentral/aamp

commit bda30014bed262e5c4f72279908c98a2f1c42270

VPLAY-11225 [Tech Debt] mediafailed event enhancements ( minor code etc ) (#685)
VPLAY-11225 [Tech Debt] mediafailed event enhancements ( minor code etc )

Reason for change: Added minor code for MediaFailedEvent
Risks: Low
Test Procedure: Refer jira ticket
Priority: P1

Signed-off-by: lashmintha <lashminthaj17@gmail.com>

priv_aamp.cpp
@@ -170,53 +170,78 @@ struct CurlCbContextSyncTime
 struct TuneFailureMap
 {
     AAMPTuneFailure tuneFailure;    /**< Failure ID */
-    int code;                       /**< Error code */
+    int code;                       /**< Major Error code */
+	int subCode;					/**< Minor Error code */
     const char* description;        /**< Textual description */
 };
 
 static TuneFailureMap tuneFailureMap[] =
 {
-	{AAMP_TUNE_INIT_FAILED, 10, "AAMP: init failed"}, //"Fragmentcollector initialization failed"
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_DNLD_ERROR, 10, "AAMP: init failed (unable to download manifest)"},
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_CONTENT_ERROR, 10, "AAMP: init failed (manifest missing tracks)"},
-	{AAMP_TUNE_INIT_FAILED_MANIFEST_PARSE_ERROR, 10, "AAMP: init failed (corrupt/invalid manifest)"},
-	{AAMP_TUNE_INIT_FAILED_PLAYLIST_VIDEO_DNLD_ERROR, 10, "AAMP: init failed (unable to download video playlist)"},
-	{AAMP_TUNE_INIT_FAILED_PLAYLIST_AUDIO_DNLD_ERROR, 10, "AAMP: init failed (unable to download audio playlist)"},
-	{AAMP_TUNE_INIT_FAILED_TRACK_SYNC_ERROR, 10, "AAMP: init failed (unsynchronized tracks)"},
-	{AAMP_TUNE_MANIFEST_REQ_FAILED, 10, "AAMP: Manifest Download failed"}, //"Playlist refresh failed"
-	{AAMP_TUNE_AUTHORIZATION_FAILURE, 40, "AAMP: Authorization failure"},
-	{AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE, 10, "AAMP: fragment download failures"},
-	{AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE, 10, "AAMP: init fragment download failed"},
-	{AAMP_TUNE_UNTRACKED_DRM_ERROR, 50, "AAMP: DRM error untracked error"},
-	{AAMP_TUNE_DRM_INIT_FAILED, 50, "AAMP: DRM Initialization Failed"},
-	{AAMP_TUNE_DRM_DATA_BIND_FAILED, 50, "AAMP: InitData-DRM Binding Failed"},
-	{AAMP_TUNE_DRM_SESSIONID_EMPTY, 50, "AAMP: DRM Session ID Empty"},
-	{AAMP_TUNE_DRM_CHALLENGE_FAILED, 50, "AAMP: DRM License Challenge Generation Failed"},
-	{AAMP_TUNE_LICENCE_TIMEOUT, 50, "AAMP: DRM License Request Timed out"},
-	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, "AAMP: DRM License Request Failed"},
-	{AAMP_TUNE_INVALID_DRM_KEY, 50, "AAMP: Invalid Key Error, from DRM"},
-	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"
-	{AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE, 60, "AAMP: No supported Audio Types in Manifest"},
-	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, "AAMP: Failed to parse key id from PSSH"},
-	{AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN, 50, "AAMP: Failed to get access token from Auth Service"},
-	{AAMP_TUNE_CORRUPT_DRM_DATA, 51, "AAMP: DRM failure due to Corrupt DRM files"},
-	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
-	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, "AAMP: DRM Decryption Failed for Fragments"},
-	{AAMP_TUNE_DRM_UNSUPPORTED, 50, "AAMP: DRM format Unsupported"},
-	{AAMP_TUNE_DRM_SELF_ABORT, 50, "AAMP: DRM license request aborted by player"},
-	{AAMP_TUNE_GST_PIPELINE_ERROR, 80, "AAMP: Error from gstreamer pipeline"},
-	{AAMP_TUNE_PLAYBACK_STALLED, 7600, "AAMP: Playback was stalled due to lack of new fragments"},
-	{AAMP_TUNE_CONTENT_NOT_FOUND, 20, "AAMP: Resource was not found at the URL(HTTP 404)"},
-	{AAMP_TUNE_DRM_KEY_UPDATE_FAILED, 50, "AAMP: Failed to process DRM key"},
-	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 52, "AAMP: Device not provisioned"},
-	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 53, "AAMP: HDCP Compliance Check Failure"},
-	{AAMP_TUNE_INVALID_MANIFEST_FAILURE, 10, "AAMP: Invalid Manifest, parse failed"},
-	{AAMP_TUNE_FAILED_PTS_ERROR, 80, "AAMP: Playback failed due to PTS error"},
-	{AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING, 10, "AAMP: init fragments missing in playlist"},
-	{AAMP_TUNE_DNS_RESOLVE_TIMEOUT, 10, "AAMP: Manifest download failed due to DNS resolve timeout"},
-	{AAMP_TUNE_CURL_CONNECTION_TIMEOUT, 10, "AAMP: Manifest download failed due to connection timeout"},
-	{AAMP_TUNE_DATA_TRANSFER_TIMEOUT, 10, "AAMP: Manifest download failed due to data transfer timeout"},
-	{AAMP_TUNE_FAILURE_UNKNOWN, 100, "AAMP: Unknown Failure"}
+	//Init failure
+	{AAMP_TUNE_INIT_FAILED, 10, 1, "AAMP: init failed"}, //"Fragmentcollector initialization failed"
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_DNLD_ERROR, 10, 2, "AAMP: init failed (unable to download manifest)"},
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_CONTENT_ERROR, 10, 3, "AAMP: init failed (manifest missing tracks)"},
+	{AAMP_TUNE_INIT_FAILED_MANIFEST_PARSE_ERROR, 10, 4, "AAMP: init failed (corrupt/invalid manifest)"},
+	{AAMP_TUNE_INIT_FAILED_PLAYLIST_VIDEO_DNLD_ERROR, 10, 5, "AAMP: init failed (unable to download video playlist)"},
+	{AAMP_TUNE_INIT_FAILED_PLAYLIST_AUDIO_DNLD_ERROR, 10, 6, "AAMP: init failed (unable to download audio playlist)"},
+	{AAMP_TUNE_INIT_FAILED_TRACK_SYNC_ERROR, 10, 7, "AAMP: init failed (unsynchronized tracks)"},
+	
+	
+    //Resource failure
+	{AAMP_TUNE_CONTENT_NOT_FOUND, 20, 1, "AAMP: Resource was not found at the URL(HTTP 404)"},
+	
+	//Download failure
+	{AAMP_TUNE_MANIFEST_REQ_FAILED, 30, 1, "AAMP: Manifest Download failed"}, //"Playlist refresh failed"
+	{AAMP_TUNE_FRAGMENT_DOWNLOAD_FAILURE, 30, 2, "AAMP: fragment download failures"},
+	{AAMP_TUNE_INIT_FRAGMENT_DOWNLOAD_FAILURE, 30, 3, "AAMP: init fragment download failed"},
+	{AAMP_TUNE_INVALID_MANIFEST_FAILURE, 30, 4, "AAMP: Invalid Manifest, parse failed"},
+	{AAMP_TUNE_MP4_INIT_FRAGMENT_MISSING, 30, 5, "AAMP: init fragments missing in playlist"},
+	{AAMP_TUNE_DNS_RESOLVE_TIMEOUT, 30, 6, "AAMP: Manifest download failed due to DNS resolve timeout"},
+	{AAMP_TUNE_CURL_CONNECTION_TIMEOUT, 30, 7, "AAMP: Manifest download failed due to connection timeout"},
+	{AAMP_TUNE_DATA_TRANSFER_TIMEOUT, 30, 8, "AAMP: Manifest download failed due to data transfer timeout"},
+
+
+	//Authorization failure
+	{AAMP_TUNE_AUTHORIZATION_FAILURE, 40, 1, "AAMP: Authorization failure"},
+
+	//DRM Failure
+	{AAMP_TUNE_UNTRACKED_DRM_ERROR, 50, 1, "AAMP: DRM error untracked error"},
+	{AAMP_TUNE_DRM_INIT_FAILED, 50, 2, "AAMP: DRM Initialization Failed"},
+	{AAMP_TUNE_DRM_DATA_BIND_FAILED, 50, 3, "AAMP: InitData-DRM Binding Failed"},
+	{AAMP_TUNE_DRM_SESSIONID_EMPTY, 50, 4, "AAMP: DRM Session ID Empty"},
+	{AAMP_TUNE_DRM_CHALLENGE_FAILED, 50, 5, "AAMP: DRM License Challenge Generation Failed"},
+	{AAMP_TUNE_LICENCE_TIMEOUT, 50, 6, "AAMP: DRM License Request Timed out"},
+	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, 7, "AAMP: DRM License Request Failed"},
+	{AAMP_TUNE_INVALID_DRM_KEY, 50, 8, "AAMP: Invalid Key Error, from DRM"},
+	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, 9, "AAMP: Failed to parse key id from PSSH"},
+	{AAMP_TUNE_CORRUPT_DRM_DATA, 50, 10, "AAMP: DRM failure due to Corrupt DRM files"},
+	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, 11, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
+	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, 12, "AAMP: DRM Decryption Failed for Fragments"},
+	{AAMP_TUNE_DRM_UNSUPPORTED, 50, 13, "AAMP: DRM format Unsupported"},
+	{AAMP_TUNE_DRM_SELF_ABORT, 50, 14, "AAMP: DRM license request aborted by player"},
+	{AAMP_TUNE_FAILED_TO_GET_ACCESS_TOKEN, 50, 15, "AAMP: Failed to get access token from Auth Service"},
+	{AAMP_TUNE_DRM_KEY_UPDATE_FAILED, 50, 16, "AAMP: Failed to process DRM key"},
+
+	
+	//Provisioning failure
+	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 51, 1, "AAMP: Device not provisioned"},
+
+	//Hdcp failure
+	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 52, 1, "AAMP: HDCP Compliance Check Failure"},
+
+	//Stream failure
+	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, 1, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"
+	{AAMP_TUNE_UNSUPPORTED_AUDIO_TYPE, 60, 2, "AAMP: No supported Audio Types in Manifest"},
+
+	//Gstreamer error
+	{AAMP_TUNE_GST_PIPELINE_ERROR, 80, 1, "AAMP: Error from gstreamer pipeline"},
+	{AAMP_TUNE_FAILED_PTS_ERROR, 80, 2, "AAMP: Playback failed due to PTS error"},
+
+	//Playback failure
+	{AAMP_TUNE_PLAYBACK_STALLED, 7600, 1, "AAMP: Playback was stalled due to lack of new fragments"},
+	
+	//Unknown failure
+	{AAMP_TUNE_FAILURE_UNKNOWN, 100, 1, "AAMP: Unknown Failure"}
 };
 
 static const std::pair<std::string , std::string> gCDAIErrorDetails[] = {
@@ -3011,7 +3036,7 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 	lock.unlock();
 	if (sendErrorEvent)
 	{
-		int code;
+		int code = AAMP_TUNE_FAILURE_UNKNOWN, subCode = 1;
 		const char *errorDescription = NULL;
 		DisableDownloads();
 		if(tuneFailure >= 0 && tuneFailure < AAMP_TUNE_FAILURE_UNKNOWN)
@@ -3024,6 +3049,7 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 			{
 				code = tuneFailureMap[tuneFailure].code;
 			}
+			subCode = tuneFailureMap[tuneFailure].subCode;
 			if(description)
 			{
 				errorDescription = description;
@@ -3036,17 +3062,18 @@ void PrivateInstanceAAMP::SendErrorEvent(AAMPTuneFailure tuneFailure, const char
 		else
 		{
 			code = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].code;
+			subCode = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].subCode;
 			errorDescription = tuneFailureMap[AAMP_TUNE_FAILURE_UNKNOWN].description;
 		}
-		MediaErrorEventPtr e = std::make_shared<MediaErrorEvent>(tuneFailure, code, errorDescription, isRetryEnabled, secManagerClassCode, secManagerReasonCode, secClientBusinessStatus, responseData, GetSessionId());
+		MediaErrorEventPtr e = std::make_shared<MediaErrorEvent>(tuneFailure, code, subCode, errorDescription, isRetryEnabled, secManagerClassCode, secManagerReasonCode, secClientBusinessStatus, responseData, GetSessionId());
 		SendAnomalyEvent(ANOMALY_ERROR, "Error[%d]:%s", tuneFailure, e->getDescription().c_str());
 		if (!mAppName.empty())
 		{
-			AAMPLOG_ERR("%s PLAYER[%d] APP: %s Sending error %s",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, mAppName.c_str(), e->getDescription().c_str());
+			AAMPLOG_ERR("%s PLAYER[%d] APP: %s Sending error %s code [%d:%d]",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, mAppName.c_str(), e->getDescription().c_str(),code, subCode);
 		}
 		else
 		{
-			AAMPLOG_ERR("%s PLAYER[%d] Sending error %s",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, e->getDescription().c_str());
+			AAMPLOG_ERR("%s PLAYER[%d] Sending error %s code [%d:%d]",(mbPlayEnabled?STRFGPLAYER:STRBGPLAYER), mPlayerId, e->getDescription().c_str(), code, subCode);
 		}
 
 		if (rate != AAMP_NORMAL_PLAY_RATE)

test/utests/fakes/FakeAampEvent.cpp
@@ -328,7 +328,7 @@ int SupportedSpeedsChangedEvent::getSupportedSpeedCount() const
 	return 0;
 }
 
-MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
+MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
 		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid))
 {
 }
@@ -336,6 +336,7 @@ bool MediaErrorEvent::shouldRetry(void) const { return false; }
 int32_t MediaErrorEvent::getBusinessStatus(void) const { return 0; }
 int32_t MediaErrorEvent::getClass(void) const { return 0; }
 int MediaErrorEvent::getCode(void) const { return 0; }
+int MediaErrorEvent::getSubCode(void) const { return 0; }
 
 BitrateChangeEvent::BitrateChangeEvent(int time, BitsPerSecond bitrate, const std::string &desc, int width, int height, double frameRate, double position, bool cappedProfile, int displayWidth, int displayHeight, VideoScanType videoScanType, int aspectRatioWidth, int aspectRatioHeight, std::string sid):
 		AAMPEventObject(AAMP_EVENT_BITRATE_CHANGED, std::move(sid))

AampEvent.cpp
@@ -49,8 +49,8 @@ AAMPEventType AAMPEventObject::getType() const
 /**
  * @brief MediaErrorEvent Constructor
  */
-MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
-		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid)), mFailure(failure), mCode(code),
+MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
+		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid)), mFailure(failure), mCode(code), mSubCode(subCode),
 		mDescription(desc), mShouldRetry(shouldRetry), mSecManagerClass(classCode), mSecManagerReasonCode(reason), mBusinessStatus(businessStatus), mResponseData(responseData)
 {
 
@@ -67,15 +67,23 @@ AAMPTuneFailure MediaErrorEvent::getFailure() const
 }
 
 /**
- * @brief Get Error Code
- *
- * @return Tune error code
+ * @brief Get the error sub code associated with this media error event.
+ * 
+ * @return Error sub code value for this media error.
  */
 int MediaErrorEvent::getCode() const
 {
 	return mCode;
 }
-
+/**
+ * @brief Get the error code associated with this media error event.
+ * 
+ * @return Error code value for this media error
+ */
+int MediaErrorEvent::getSubCode() const
+{
+	return mSubCode;
+}
 /**
  * @brief Get Description
  *

commit b442ed4a585a43f505cc29028ccf2a600801cc25

VPAAMP-918  : Fix error code regression: restore backward compatibilty for codes 51–53 (#1823)
* VPAAMP-918  : Fix error code regression: restore backward compatibility for codes 51–53

Reason for change:

Commit bda30014 (VPLAY-11225) consolidated CORRUPT_DRM_DATA into the DRM error
group (50/10), inadvertently cascading a −1 shift on codes 52 and 53. This broke
the published API contract for external clients matching on error codes.

Restore original major codes while retaining subCode field:
- CORRUPT_DRM_DATA:       50,10 → 51,1
- DEVICE_NOT_PROVISIONED: 51,1 → 52,1
- HDCP_COMPLIANCE_ERROR:  52,1 → 53,1

External consumers matching event.code === 51|52|53 now work correctly again.
Internal DRM error grouping semantics preserved via subCode field.

Fixes: VPLAY-11225 (regression from bda30014)

Test Procedure:

Priority: P2

Risks:Low

* VPAAMP-918  :docs: update AAMP-UVE-API.md error code table to reflect restored codes

Reason for change:

Update error code table to document the restored backward-compatible major
codes following the fix to priv_aamp.cpp:
- CORRUPT_DRM_DATA:       50|10 → 51|1
- DEVICE_NOT_PROVISIONED: 51|1 → 52|1
- HDCP_COMPLIANCE_ERROR:  52|1 → 53|1

Keeps published API documentation in sync with the implementation.
Relates: error code regression fix for backward compatibility

Test Procedure:

Priority: P2

Risks:Low

* VPAAMP-918  : test: add L1 tests for error code mapping verification

Reason for change:

Add comprehensive test coverage for the backward-compatible error codes
restored in VPAAMP-918. Tests lock in the API contract to prevent future
regressions on codes 51, 52, 53.

Test cases:
- CorruptDrmData_ReturnsCode51SubCode1
- DeviceNotProvisioned_ReturnsCode52SubCode1
- HdcpCompliance_ReturnsCode53SubCode1
- ErrorCodeSequence_AllThreeCodesUnique

Tests ensure external clients matching on event.code === 51|52|53
continue to work correctly

Test Procedure:

Priority: P2

Risks:Low

priv_aamp.cpp
@@ -222,7 +222,7 @@ static TuneFailureMap tuneFailureMap[] =
 	{AAMP_TUNE_LICENCE_REQUEST_FAILED, 50, 7, "AAMP: DRM License Request Failed"},
 	{AAMP_TUNE_INVALID_DRM_KEY, 50, 8, "AAMP: Invalid Key Error, from DRM"},
 	{AAMP_TUNE_FAILED_TO_GET_KEYID, 50, 9, "AAMP: Failed to parse key id from PSSH"},
-	{AAMP_TUNE_CORRUPT_DRM_DATA, 50, 10, "AAMP: DRM failure due to Corrupt DRM files"},
+	{AAMP_TUNE_CORRUPT_DRM_DATA, 51, 1, "AAMP: DRM failure due to Corrupt DRM files"},
 	{AAMP_TUNE_CORRUPT_DRM_METADATA, 50, 11, "AAMP: DRM failure due to Bad DRMMetadata in stream"},
 	{AAMP_TUNE_DRM_DECRYPT_FAILED, 50, 12, "AAMP: DRM Decryption Failed for Fragments"},
 	{AAMP_TUNE_DRM_UNSUPPORTED, 50, 13, "AAMP: DRM format Unsupported"},
@@ -233,10 +233,10 @@ static TuneFailureMap tuneFailureMap[] =
 
 
 	//Provisioning failure
-	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 51, 1, "AAMP: Device not provisioned"},
+	{AAMP_TUNE_DEVICE_NOT_PROVISIONED, 52, 1, "AAMP: Device not provisioned"},
 
 	//Hdcp failure
-	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 52, 1, "AAMP: HDCP Compliance Check Failure"},
+	{AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 53, 1, "AAMP: HDCP Compliance Check Failure"},
 
 	//Stream failure
 	{AAMP_TUNE_UNSUPPORTED_STREAM_TYPE, 60, 1, "AAMP: Unsupported Stream Type"}, //"Unable to determine stream type for DRM Init"

test/utests/fakes/FakeAampEvent.cpp
@@ -348,14 +348,14 @@ int SupportedSpeedsChangedEvent::getSupportedSpeedCount() const
 }
 
 MediaErrorEvent::MediaErrorEvent(AAMPTuneFailure failure, int code, int subCode, const std::string &desc, bool shouldRetry, int classCode, int reason, int businessStatus, const std::string &responseData, std::string sid):
-		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid))
+		AAMPEventObject(AAMP_EVENT_TUNE_FAILED, std::move(sid)), mCode(code), mSubCode(subCode)
 {
 }
 bool MediaErrorEvent::shouldRetry(void) const { return false; }
 int32_t MediaErrorEvent::getBusinessStatus(void) const { return 0; }
 int32_t MediaErrorEvent::getClass(void) const { return 0; }
-int MediaErrorEvent::getCode(void) const { return 0; }
-int MediaErrorEvent::getSubCode(void) const { return 0; }
+int MediaErrorEvent::getCode(void) const { return mCode; }
+int MediaErrorEvent::getSubCode(void) const { return mSubCode; }
 
 BitrateChangeEvent::BitrateChangeEvent(int time, BitsPerSecond bitrate, const std::string &desc, int width, int height, double frameRate, double position, bool cappedProfile, int displayWidth, int displayHeight, VideoScanType videoScanType, int aspectRatioWidth, int aspectRatioHeight, std::string sid):
 		AAMPEventObject(AAMP_EVENT_BITRATE_CHANGED, std::move(sid))

test/utests/tests/AampEventTests/CMakeLists.txt
@@ -25,7 +25,8 @@ set(EXEC_NAME AampEventTests)
 include(${CMAKE_CURRENT_LIST_DIR}/../CommonTestIncludes.cmake)
 
 set(TEST_SOURCES AampEventTests.cpp
-AampEventAAMPTests.cpp)
+AampEventAAMPTests.cpp
+ErrorCodeMappingTests.cpp)
 set(AAMP_SOURCES ${AAMP_ROOT}/AampEvent.cpp)
 add_executable(${EXEC_NAME}
                ${TEST_SOURCES}

test/utests/tests/AampEventTests/ErrorCodeMappingTests.cpp
@@ -1,0 +1,157 @@
+/*
+ * If not stated otherwise in this file or this component's license file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2026 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#include <gtest/gtest.h>
+#include "AampEvent.h"
+
+using namespace testing;
+
+/**
+ * @class ErrorCodeMappingTests
+ * @brief Tests that specific tune failures map to their documented error codes.
+ *
+ * Purpose: Lock in the backward-compatible error code contract (VPAAMP-918).
+ * These tests verify that the three error codes (51, 52, 53) that were
+ * inadvertently shifted by commit bda30014 are now restored to their
+ * original values, allowing external clients to match on these codes.
+ *
+ * Specification: See AAMP-UVE-API.md error code table for documented values.
+ * Related: Commit bda30014 (VPLAY-11225) caused regression by consolidating
+ * CORRUPT_DRM_DATA into the DRM group (50/10), cascading shifts to codes
+ * 52 and 53. This PR restores codes 51, 52, 53 while keeping subCode for
+ * fine-grained categorization.
+ */
+
+class ErrorCodeMappingTests : public testing::Test {
+protected:
+	const std::string session_id{"test-session-id-error-code-mapping"};
+
+	// Test helper: creates MediaErrorEvent and verifies code/subCode
+	void VerifyErrorCodeMapping(
+		AAMPTuneFailure tuneFailure,
+		int expectedCode,
+		int expectedSubCode,
+		const char* description)
+	{
+		MediaErrorEvent event(
+			tuneFailure,
+			expectedCode,
+			expectedSubCode,
+			description,
+			false,
+			0,
+			0,
+			0,
+			"",
+			session_id
+		);
+
+		EXPECT_EQ(event.getCode(), expectedCode)
+			<< "Tune failure " << static_cast<int>(tuneFailure)
+			<< " should map to code " << expectedCode;
+		EXPECT_EQ(event.getSubCode(), expectedSubCode)
+			<< "Tune failure " << static_cast<int>(tuneFailure)
+			<< " should map to subCode " << expectedSubCode;
+	}
+};
+
+/**
+ * TEST: CorruptDrmData_ReturnsCode51SubCode1
+ *
+ * Ensures AAMP_TUNE_CORRUPT_DRM_DATA maps to error code 51, subCode 1.
+ *
+ * Regression prevention: Commit bda30014 moved this from code 51 to 50/10.
+ * External clients matching event.code === 51 would fail silently.
+ * This test locks in the restored backward-compatible code.
+ */
+TEST_F(ErrorCodeMappingTests, CorruptDrmData_ReturnsCode51SubCode1)
+{
+	VerifyErrorCodeMapping(
+		AAMP_TUNE_CORRUPT_DRM_DATA,
+		51,  // Expected major code (restored from 50 in bda30014)
+		1,   // Expected subCode
+		"AAMP: DRM failure due to Corrupt DRM files"
+	);
+}
+
+/**
+ * TEST: DeviceNotProvisioned_ReturnsCode52SubCode1
+ *
+ * Ensures AAMP_TUNE_DEVICE_NOT_PROVISIONED maps to error code 52, subCode 1.
+ *
+ * Regression prevention: Commit bda30014 cascaded a −1 shift, moving this
+ * from code 52 to 51. External clients matching code 52 would fail silently.
+ * This test locks in the restored backward-compatible code.
+ */
+TEST_F(ErrorCodeMappingTests, DeviceNotProvisioned_ReturnsCode52SubCode1)
+{
+	VerifyErrorCodeMapping(
+		AAMP_TUNE_DEVICE_NOT_PROVISIONED,
+		52,  // Expected major code (restored from 51 in bda30014)
+		1,   // Expected subCode
+		"AAMP: Device not provisioned"
+	);
+}
+
+/**
+ * TEST: HdcpComplianceError_ReturnsCode53SubCode1
+ *
+ * Ensures AAMP_TUNE_HDCP_COMPLIANCE_ERROR maps to error code 53, subCode 1.
+ *
+ * Regression prevention: Commit bda30014 cascaded a −1 shift, moving this
+ * from code 53 to 52. External clients matching code 53 would fail silently.
+ * This test locks in the restored backward-compatible code.
+ */
+TEST_F(ErrorCodeMappingTests, HdcpCompliance_ReturnsCode53SubCode1)
+{
+	VerifyErrorCodeMapping(
+		AAMP_TUNE_HDCP_COMPLIANCE_ERROR,
+		53,  // Expected major code (restored from 52 in bda30014)
+		1,   // Expected subCode
+		"AAMP: HDCP Compliance Check Failure"
+	);
+}
+
+/**
+ * TEST: ErrorCodeSequence_AllThreeCodesUnique
+ *
+ * Ensures that the three restored error codes are unique and sequential.
+ * This guards against accidental collisions or reversions.
+ */
+TEST_F(ErrorCodeMappingTests, ErrorCodeSequence_AllThreeCodesUnique)
+{
+	MediaErrorEvent event1(
+		AAMP_TUNE_CORRUPT_DRM_DATA, 51, 1, "", false, 0, 0, 0, "", session_id
+	);
+	MediaErrorEvent event2(
+		AAMP_TUNE_DEVICE_NOT_PROVISIONED, 52, 1, "", false, 0, 0, 0, "", session_id
+	);
+	MediaErrorEvent event3(
+		AAMP_TUNE_HDCP_COMPLIANCE_ERROR, 53, 1, "", false, 0, 0, 0, "", session_id
+	);
+
+	// Verify uniqueness
+	EXPECT_NE(event1.getCode(), event2.getCode());
+	EXPECT_NE(event2.getCode(), event3.getCode());
+	EXPECT_NE(event1.getCode(), event3.getCode());
+
+	// Verify sequential relationship for external validation
+	EXPECT_EQ(event1.getCode() + 1, event2.getCode());
+	EXPECT_EQ(event2.getCode() + 1, event3.getCode());
+}

test/utests/tests/PrivAampTests/PrivAampTests.cpp
@@ -6592,3 +6592,98 @@ TEST_F(PrivAampPrivTests, StartLatencyMonitor_LiveLatencyCorrectionDisabled_Does
 	testp_aamp->mpStreamAbstractionAAMP = nullptr;
 }
 
+// ---------------------------------------------------------------------------
+// VPAAMP-918: Error code mapping contract tests
+//
+// These tests verify the end-to-end path through SendErrorEvent() →
+// tuneFailureMap[] lookup → MediaErrorEvent emission for the three error
+// codes that were inadvertently shifted by commit bda30014 (VPLAY-11225).
+//
+// The developer's AampEventTests/ErrorCodeMappingTests.cpp tests only the
+// MediaErrorEvent value-holder constructor (circular: it passes in the
+// expected code and asserts it back out).  These tests exercise the real
+// tuneFailureMap[] table in priv_aamp.cpp so that any future modification
+// to that table causes an immediate CI failure.
+//
+// Oracle:
+//   AAMP_TUNE_CORRUPT_DRM_DATA       → code 51, subCode 1
+//   AAMP_TUNE_DEVICE_NOT_PROVISIONED → code 52, subCode 1
+//   AAMP_TUNE_HDCP_COMPLIANCE_ERROR  → code 53, subCode 1
+// ---------------------------------------------------------------------------
+
+/**
+ * @test SendErrorEvent_CorruptDrmData_EmitsCode51SubCode1
+ * @brief SendErrorEvent(AAMP_TUNE_CORRUPT_DRM_DATA) must produce a
+ *        MediaErrorEvent with code==51, subCode==1.
+ *
+ * Regression guard for VPAAMP-918 / bda30014: CORRUPT_DRM_DATA was moved
+ * to 50/10 by that commit, breaking external clients matching code 51.
+ * This test fails if tuneFailureMap[] is reverted or shifted again.
+ *
+ * Rate is set to AAMP_NORMAL_PLAY_RATE so that SendErrorEvent does not
+ * also emit a speed-change event before the MediaErrorEvent.
+ */
+TEST_F(PrivAampTests, SendErrorEvent_CorruptDrmData_EmitsCode51SubCode1)
+{
+	p_aamp->rate = AAMP_NORMAL_PLAY_RATE;
+	AAMPEventPtr capturedEvent;
+	EXPECT_CALL(*g_mockAampEventManager, SendEvent(AnEventOfType(AAMP_EVENT_TUNE_FAILED), AAMP_EVENT_ASYNC_MODE))
+		.WillOnce(::testing::SaveArg<0>(&capturedEvent));
+
+	p_aamp->SendErrorEvent(AAMP_TUNE_CORRUPT_DRM_DATA);
+
+	ASSERT_NE(capturedEvent, nullptr);
+	MediaErrorEventPtr errorEvent = std::dynamic_pointer_cast<MediaErrorEvent>(capturedEvent);
+	ASSERT_NE(errorEvent, nullptr);
+	EXPECT_EQ(errorEvent->getCode(),    51);
+	EXPECT_EQ(errorEvent->getSubCode(),  1);
+}
+
+/**
+ * @test SendErrorEvent_DeviceNotProvisioned_EmitsCode52SubCode1
+ * @brief SendErrorEvent(AAMP_TUNE_DEVICE_NOT_PROVISIONED) must produce a
+ *        MediaErrorEvent with code==52, subCode==1.
+ *
+ * Regression guard for VPAAMP-918 / bda30014: the −1 cascade shifted this
+ * from 52/1 to 51/1, breaking external clients matching code 52.
+ */
+TEST_F(PrivAampTests, SendErrorEvent_DeviceNotProvisioned_EmitsCode52SubCode1)
+{
+	p_aamp->rate = AAMP_NORMAL_PLAY_RATE;
+	AAMPEventPtr capturedEvent;
+	EXPECT_CALL(*g_mockAampEventManager, SendEvent(AnEventOfType(AAMP_EVENT_TUNE_FAILED), AAMP_EVENT_ASYNC_MODE))
+		.WillOnce(::testing::SaveArg<0>(&capturedEvent));
+
+	p_aamp->SendErrorEvent(AAMP_TUNE_DEVICE_NOT_PROVISIONED);
+
+	ASSERT_NE(capturedEvent, nullptr);
+	MediaErrorEventPtr errorEvent = std::dynamic_pointer_cast<MediaErrorEvent>(capturedEvent);
+	ASSERT_NE(errorEvent, nullptr);
+	EXPECT_EQ(errorEvent->getCode(),    52);
+	EXPECT_EQ(errorEvent->getSubCode(),  1);
+}
+
+/**
+ * @test SendErrorEvent_HdcpComplianceError_EmitsCode53SubCode1
+ * @brief SendErrorEvent(AAMP_TUNE_HDCP_COMPLIANCE_ERROR) must produce a
+ *        MediaErrorEvent with code==53, subCode==1.
+ *
+ * Regression guard for VPAAMP-918 / bda30014: the −1 cascade shifted this
+ * from 53/1 to 52/1, breaking external clients matching code 53.
+ */
+TEST_F(PrivAampTests, SendErrorEvent_HdcpComplianceError_EmitsCode53SubCode1)
+{
+	p_aamp->rate = AAMP_NORMAL_PLAY_RATE;
+	AAMPEventPtr capturedEvent;
+	EXPECT_CALL(*g_mockAampEventManager, SendEvent(AnEventOfType(AAMP_EVENT_TUNE_FAILED), AAMP_EVENT_ASYNC_MODE))
+		.WillOnce(::testing::SaveArg<0>(&capturedEvent));
+
+	p_aamp->SendErrorEvent(AAMP_TUNE_HDCP_COMPLIANCE_ERROR);
+
+	ASSERT_NE(capturedEvent, nullptr);
+	MediaErrorEventPtr errorEvent = std::dynamic_pointer_cast<MediaErrorEvent>(capturedEvent);
+	ASSERT_NE(errorEvent, nullptr);
+	EXPECT_EQ(errorEvent->getCode(),    53);
+	EXPECT_EQ(errorEvent->getSubCode(),  1);
+}
+