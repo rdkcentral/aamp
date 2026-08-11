55909745e66038a695d79082f983aac2335bf313
anshephe <115161257+anshephe@users.noreply.github.com>
2026-08-11 21:29:56 +0000

    Removed eAAMPConfig_RialtoSinkSetsDirectRialto usage now that tests can set the config

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -364,7 +364,6 @@ static const ConfigLookupEntryBool mConfigLookupTableBool[AAMPCONFIG_BOOL_COUNT]
 	{false, "seamlessAudioSwitch", eAAMPConfig_SeamlessAudioSwitch, true},
 	{false, "useRialtoSink", eAAMPConfig_useRialtoSink, false},
 	{false, "useDirectRialto", eAAMPConfig_useDirectRialto, false},
-	{true, "RialtoSinkSetsDirectRialto", eAAMPConfig_RialtoSinkSetsDirectRialto, false},
 	{false, "localTSBEnabled", eAAMPConfig_LocalTSBEnabled, true},
 	{false, "enableIFrameTrackExtract", eAAMPConfig_EnableIFrameTrackExtract, true},
 	{false, "forceMultiPeriodDiscontinuity", eAAMPConfig_ForceMultiPeriodDiscontinuity, false},
