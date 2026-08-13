7ca049e4cb387191a5bcad02146009afacfba310
DomSyna <192202460+DomSyna@users.noreply.github.com>
2026-08-13 18:36:59 +0100

    Refactor error logging for MP4 segment parsing

--- mp4demux/AampMp4Demuxer.cpp
+++ mp4demux/AampMp4Demuxer.cpp
@@ -263,8 +263,8 @@ bool AampMp4Demuxer::sendSegment(std::vector<uint8_t>&& buffer, double position,
 		if (!ret)
 		{
 			AAMPLOG_ERR("Failed to parse MP4 segment [err:%d] for type:%d position: %f, duration: %f, isInit: %d", mMp4Demux->GetLastError(), mMediaType, position, duration, isInit);
-			mAamp->SendErrorEvent(AAMP_TUNE_MP4_DEMUX_ERROR,
-				"Mp4Demux Error:This file is invalid and cannot be played.", false);		}
+			mAamp->SendErrorEvent(AAMP_TUNE_MP4_DEMUX_ERROR, "Mp4Demux Error:This file is invalid and cannot be played.", false);		
+		}
 		else
 		{
 			auto samples = mMp4Demux->GetSamples();
