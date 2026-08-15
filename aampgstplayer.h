f149597b5e2ab6ba774a8479087614a7aaa3e10f
nejuma1 <nejumatn28@gmail.com>
2026-08-15 19:41:13 +0530

    Update aampgstplayer.h

--- aampgstplayer.h
+++ aampgstplayer.h
@@ -168,7 +168,7 @@ public:
 		 * @param[in] shouldTearDown flag indicates if pipeline should be destroyed if in invalid state
 		 * @param[in] keepPausedSeek true only for an explicit seek-with-keepPaused request
 		 */
-	void Flush(double position, int rate, bool shouldTearDown, bool keepPausedSeek) override;
+	void Flush(double position, int rate, bool shouldTearDown, bool keepPausedSeek);
 	/**
 		 * @fn Pause
 		 * @param[in] pause flag to pause/play the pipeline
