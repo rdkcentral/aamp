e1612fb810457a166332f3aab0e78472540f2dae
Philip Stroffolino <philip_stroffolino@cable.comcast.com>
2026-08-17 11:45:26 -0400

    Reason for Change: added default useMp4Demux on top of logging patch VPAAMP-1017 to avoid massive l2 breakage
    Signed-off-by: Philip Stroffolino <philip_stroffolino@cable.comcast.com>

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -378,7 +378,7 @@ static const ConfigLookupEntryBool mConfigLookupTableBool[AAMPCONFIG_BOOL_COUNT]
 	{false, "debugChunkTransfer", eAAMPConfig_DebugChunkTransfer, false},
 	{true, "utcSyncOnStartup", eAAMPConfig_UTCSyncOnStartup, true},
 	{false, "disableWebVTT", eAAMPConfig_DisableWebVTT, false},
-	{false, "enablePTSReStampLogging", eAAMPConfig_EnablePTSReStampLogging, false},
+	{true, "enablePTSReStampLogging", eAAMPConfig_EnablePTSReStampLogging, false},
 	{false, "netTraceCsvDump", eAAMPConfig_NetTraceCsvDump, false},
 	{false, "logFilename", eAAMPConfig_LogFilename, false},
 	{false, "processLicenseFromEAP", eAAMPConfig_ProcessLicenseFromEAP, false},
