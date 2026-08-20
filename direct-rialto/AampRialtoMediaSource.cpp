dfba9cde2827223ca9d3b46284280305a1426ae0
anshephe <115161257+anshephe@users.noreply.github.com>
2026-08-20 16:14:46 +0000

    Revert "Trying with resetTime = 0 on attaching sources"
    This reverts commit 0a0493777efce9af0bea95cc81d6c073a2559e57.

--- direct-rialto/AampRialtoMediaSource.cpp
+++ direct-rialto/AampRialtoMediaSource.cpp
@@ -485,7 +485,7 @@ AampRialtoMediaSource::AttachResult AampRialtoMediaSource::attachOrUpdate(
 	if (flushPosNs >= 0)
 	{
 		if (!pipeline.setSourcePosition(
-				m_sourceId, flushPosNs, /*resetTime=*/false, appliedRate))
+				m_sourceId, flushPosNs, /*resetTime=*/true, appliedRate))
 		{
 			AAMPLOG_WARN("setSourcePosition(%" PRId64 ") appliedRate=%f failed "
 				"for mediaType=%d", flushPosNs, appliedRate,
