4624e384987292885be67d9f3dfb9d33ff8ba19a
Gavin Parry <gavin_parry@comcast.com>
2026-09-08 16:36:38 +0000

    VPAAMP-965 - filter IsAsyncTuneSupportedForType for new tune types only

--- priv_aamp.cpp
+++ priv_aamp.cpp
@@ -5606,6 +5606,7 @@ bool PrivateInstanceAAMP::IsAsyncTuneSupportedForType(MediaFormat format, Conten
 {
 	return (eMEDIAFORMAT_DASH == format) &&
 	       (ContentType_LINEAR == type)  &&
+	        ((eTUNETYPE_NEW_NORMAL == mTuneType) || (eTUNETYPE_NEW_SEEK == mTuneType) || (eTUNETYPE_NEW_END == mTuneType)) && // replace with IsNewTune()
 	       mAsyncTuneEnabled;
 }
 
