eafc3c20cd7801901e2e25b309e6e413c375f5d5
varshnie <125456267+varshnie@users.noreply.github.com>
2026-09-25 11:17:31 +0530

    Update AampConfig.cpp
    eAAMPConfig_GStreamerBufferingBeforePlay config set to false by default.

--- AampConfig.cpp
+++ AampConfig.cpp
@@ -260,7 +260,7 @@ static const ConfigLookupEntryBool mConfigLookupTableBool[AAMPCONFIG_BOOL_COUNT]
 	{true,"internalRetune",eAAMPConfig_InternalReTune,false},
 	{false,"audioOnlyPlayback",eAAMPConfig_AudioOnlyPlayback,false},
 	{false,"b64LicenseWrapping",eAAMPConfig_Base64LicenseWrapping,false},
-	{true,"gstBufferAndPlay",eAAMPConfig_GStreamerBufferingBeforePlay,false},
+	{false,"gstBufferAndPlay",eAAMPConfig_GStreamerBufferingBeforePlay,false},
 	{false,"playreadyOutputProtection",eAAMPConfig_EnablePROutputProtection,false},
 	{true,"retuneOnBufferingTimeout",eAAMPConfig_ReTuneOnBufferingTimeout,false},
 	{true,"sslVerifyPeer",eAAMPConfig_SslVerifyPeer,false},
